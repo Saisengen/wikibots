78d365b631a4af2c0c4b57d46fe45851f26ce01f
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-08-06 15:04:03 +0700

    Update 2-table-of-users.cs

--- cluster-analysis/2-table-of-users.cs
+++ cluster-analysis/2-table-of-users.cs
@@ -4,12 +4,17 @@ using System.Text.RegularExpressions;
 using System.IO;
 using System.Web;
 using System.Linq;
-
+class data
+{
+    public int diff, total;
+    public string coinciding_votings_list, opposite_votings_list;
+}
 class Program
 {
+    static bool method_is_post = true;
     static void Sendresponse(string result, string users, int earlieryear, int lateryear, string type, bool sort, bool wikidim)
     {
-        var sr = new StreamReader("clusters-template2.txt");
+        var sr = new StreamReader(method_is_post ? "clusters-template2.5.txt" : "clusters-template2.txt");
         string result1 = sr.ReadToEnd().Replace("%result%", result).Replace("%users%", users).Replace("%earlieryear%", earlieryear.ToString()).Replace("%lateryear%", lateryear.ToString());
         if (type == "d")
             result1 = result1.Replace("%checked_d%", "checked");
@@ -28,14 +33,14 @@ class Program
         var yes = new Dictionary<string, HashSet<string>>();
         var no = new Dictionary<string, HashSet<string>>();
         int counter = 0;
-        //Environment.SetEnvironmentVariable("QUERY_STRING", "users=Icannotyet%0D%0AShnapp%0D%0AЛариса94%0D%0ADubrus%0D%0AA.Morgunovskaya%0D%0AWerWoelfin%0D%0AHappyFemme%0D%0AGracerose11%0D%0AMypolik%0D%0ARainbowfem%0D%0AStraycatgirl&earlieryear=2018&lateryear=2021&type=d&wikidim=on&anticorr=0.1");
-        string get = Environment.GetEnvironmentVariable("QUERY_STRING");
-        if (get == "")
+        //Environment.SetEnvironmentVariable("QUERY_STRING", "users=MBH%0D%0Astjn%0D%0ALe+Loy%0D%0AWulfson&earlieryear=2022&lateryear=2022&type=dn");
+        string input = method_is_post ? Console.ReadLine() : Environment.GetEnvironmentVariable("QUERY_STRING");
+        if (input == "" || input == null)
         {
             Sendresponse("", "", DateTime.Now.Year, DateTime.Now.Year, "dn", false, false);
             return;
         }
-        var parameters = HttpUtility.ParseQueryString(get);
+        var parameters = HttpUtility.ParseQueryString(input);
         var users = parameters["users"].Replace("\u200E", "").Replace("\r\n", "\t").Replace("\n", "\t").Replace("\r", "\t").Split('\t');
         foreach (var u in users)
             if (u != "" && !voters.ContainsKey(u))
@@ -85,27 +90,31 @@ class Program
                     no[voting].Add(n);
         }
 
-        int[,] table = new int[voters.Count, voters.Count];
-        int[,] total = new int[voters.Count, voters.Count];
+        data[,] table = new data[voters.Count, voters.Count];
         foreach (var v1 in voters)
             foreach (var v2 in voters)
+            {
+                table[v1.Value, v2.Value] = new data();
                 foreach (var voting in votings)
                 {
                     if ((yes[voting].Contains(v1.Key) && no[voting].Contains(v2.Key)) || (no[voting].Contains(v1.Key) && yes[voting].Contains(v2.Key)))
                     {
-                        table[v1.Value, v2.Value] -= 1;
-                        total[v1.Value, v2.Value] += 1;
+                        table[v1.Value, v2.Value].diff -= 1;
+                        table[v1.Value, v2.Value].total += 1;
+                        table[v1.Value, v2.Value].opposite_votings_list += voting + '\n';
                     }
                     if ((yes[voting].Contains(v1.Key) && yes[voting].Contains(v2.Key)) || (no[voting].Contains(v1.Key) && no[voting].Contains(v2.Key)))
                     {
-                        table[v1.Value, v2.Value] += 1;
-                        total[v1.Value, v2.Value] += 1;
+                        table[v1.Value, v2.Value].diff += 1;
+                        table[v1.Value, v2.Value].total += 1;
+                        table[v1.Value, v2.Value].coinciding_votings_list += voting + '\n';
                     }
                     if (wikidim)
                         if ((yes[voting].Contains(v1.Key) || no[voting].Contains(v1.Key)) && (!no[voting].Contains(v2.Key) && !yes[voting].Contains(v2.Key)) ||
                             (yes[voting].Contains(v2.Key) || no[voting].Contains(v2.Key)) && (!no[voting].Contains(v1.Key) && !yes[voting].Contains(v1.Key)))
-                            total[v1.Value, v2.Value] += 1;
+                            table[v1.Value, v2.Value].total += 1;
                 }
+            }
 
         string result = "За указанный период времени прошло " + votings.Count + " голосований. Прочерк означает, что за этот период нет выборов, на которых проголосовали бы оба участника " +
             "(а по методу Викидима - что нет выборов, где проголосовал бы хоть один из участников).<br><br><table border=\"1\" cellspacing=\"0\"><tr><th></th>";
@@ -114,8 +123,8 @@ class Program
         {
             var sortedarray = new Dictionary<string, float>();
             foreach (var v in voters)
-                if (total[v.Value, 0] != 0)
-                    sortedarray.Add(v.Key, (float)table[v.Value, 0] / total[v.Value, 0]);
+                if (table[v.Value, 0].total != 0)
+                    sortedarray.Add(v.Key, (float)table[v.Value, 0].diff / table[v.Value, 0].total);
             foreach (var s in sortedarray.OrderByDescending(s => s.Value))
                 result += "<th>" + s.Key + "</th>\n";
             result += "</tr>";
@@ -127,16 +136,16 @@ class Program
                         result += "<td></td>";
                     else
                     {
-                        if (total[voters[s1.Key], voters[s2.Key]] != 0)
+                        if (table[voters[s1.Key], voters[s2.Key]].total != 0)
                         {
-                            float dn = (float)table[voters[s1.Key], voters[s2.Key]] / total[voters[s1.Key], voters[s2.Key]];
+                            float dn = (float)table[voters[s1.Key], voters[s2.Key]].diff / table[voters[s1.Key], voters[s2.Key]].total;
                             string antisaturation = Convert.ToInt32(Math.Round(255 * (1 - (dn > 0 ? dn : -dn)))).ToString("X2");
                             string color = (dn == 1 ? "080" : (dn < 0 ? "FF" + antisaturation + antisaturation : antisaturation + "FF" + antisaturation));
                             string dn_string = dn.ToString("G2");
                             if (dn_string.StartsWith("0.") || dn_string.StartsWith("-0."))
                                 dn_string = dn_string.Replace("0.", ".");
-                            result += "<td style=\"background-color:#" + color + (color == "080" ? "; color:white" : "") + "\"><abbr title=\"" + s1.Key + " / " + s1.Key + "\">" +
-                                (type == "d" ? table[voters[s1.Key], voters[s2.Key]].ToString() : dn_string) + "</abbr></td>\n";
+                            result += "<td style=\"background-color:#" + color + (color == "080" ? "; color:white" : "") + "\"><abbr title=\"⇈:\n" + table[voters[s1.Key], voters[s2.Key]].coinciding_votings_list + "⇅:\n" + 
+                                table[voters[s1.Key], voters[s2.Key]].opposite_votings_list + "\">" + (type == "d" ? table[voters[s1.Key], voters[s2.Key]].diff.ToString() : dn_string) + "</abbr></td>\n";
                         }
                         else
                             result += "<td><abbr title=\"" + s1.Key + " / " + s1.Key + "\">−</abbr></td>\n";
@@ -157,16 +166,17 @@ class Program
                         result += "<td></td>";
                     else
                     {
-                        if (total[v1.Value, v2.Value] != 0)
+                        if (table[v1.Value, v2.Value].total != 0)
                         {
-                            float dn = (float)table[v1.Value, v2.Value] / total[v1.Value, v2.Value];
+                            float dn = (float)table[v1.Value, v2.Value].diff / table[v1.Value, v2.Value].total;
                             string antisaturation = Convert.ToInt32(Math.Round(255 * (1 - (dn > 0 ? dn : -dn)))).ToString("X2");
                             string color = (dn == 1 ? "080" : (dn < 0 ? "FF" + antisaturation + antisaturation : antisaturation + "FF" + antisaturation));
                             string dn_string = dn.ToString("G2");
                             if (dn_string.StartsWith("0.") || dn_string.StartsWith("-0."))
                                 dn_string = dn_string.Replace("0.", ".");
-                            result += "<td style=\"background-color:#" + color + (color == "080" ? "; color:white" : "") + "\"><abbr title=\"" + v1.Key + " / " + v2.Key + "\">" +
-                                (type == "d" ? table[v1.Value, v2.Value].ToString() : dn_string) + "</abbr></td>\n";
+                            result += "<td style=\"background-color:#" + color + (color == "080" ? "; color:white" : "") + "\"><abbr title=\"⇈:\n" + table[voters[v1.Key], voters[v2.Key]].coinciding_votings_list + "⇅:\n" + 
+                                table[voters[v1.Key], voters[v2.Key]].opposite_votings_list + "\">" +
+                                (type == "d" ? table[v1.Value, v2.Value].diff.ToString() : dn_string) + "</abbr></td>\n";
                         }
                         else
                             result += "<td><abbr title=\"" + v1.Key + " / " + v2.Key + "\">−</abbr></td>\n";
