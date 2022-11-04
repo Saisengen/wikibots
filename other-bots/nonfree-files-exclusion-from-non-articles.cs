1abd747f253181ae67fcbacfce8a2a89bfd633dd
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-04 10:15:18 +0700

    Update nonfree-files-exclusion-from-non-articles.cs

--- other-bots/nonfree-files-exclusion-from-non-articles.cs
+++ other-bots/nonfree-files-exclusion-from-non-articles.cs
@@ -1,27 +1,57 @@
 using System;
 using System.Xml;
 using System.IO;
-using DotNetWikiBot;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
 
 class Program
 {
+    static HttpClient Site(string lang, string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://" + lang + ".wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://" + lang + ".wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string lang, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://" + lang + ".wikipedia.org/w/api.php", request).Result;
+        Console.WriteLine(DateTime.Now.ToString() + " writing " + lang + ":" + title);
+    }
+
     static void Main()
     {
-        string cont, query, apiout, token = "";
+        string cont, query, apiout;
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
+        var site = Site("ru", creds[0], creds[1]);
 
-        apiout = site.GetWebPage("/w/api.php?action=query&format=xml&meta=tokens&type=csrf%7Crollback");
-        using (var r = new XmlTextReader(new StringReader(apiout)))
-            while (r.Read())
-                if (r.Name == "tokens")
-                    token = Uri.EscapeDataString(r.GetAttribute("csrftoken"));
-
-        cont = ""; query = "/w/api.php?action=query&format=xml&prop=fileusage&generator=categorymembers&fuprop=title&fulimit=5000&gcmtitle=Категория:Файлы:Несвободные&gcmtype=file&gcmlimit=1000";
+        cont = ""; query = "https://ru.wikipedia.org/w/api.php?action=query&format=xml&prop=fileusage&generator=categorymembers&fuprop=title&fulimit=5000&gcmtitle=Категория:Файлы:Несвободные&gcmtype=file&gcmlimit=1000";
         while (cont != null)
         {
-            apiout = (cont == "" ? site.GetWebPage(query) : site.GetWebPage(query + "&gcmcontinue=" + Uri.EscapeDataString(cont)));
+            apiout = (cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&gcmcontinue=" + Uri.EscapeDataString(cont)).Result);
             using (var r = new XmlTextReader(new StringReader(apiout)))
             {
                 r.WhitespaceHandling = WhitespaceHandling.None;
@@ -34,9 +64,7 @@ class Program
                     if (r.Name == "fu" && r.GetAttribute("ns") != "0" && r.GetAttribute("ns") != "102")
                     {
                         string title = r.GetAttribute("title");
-                        var p = new Page(title);
-                        p.Load();
-                        string text = p.text;
+                        string text = site.GetStringAsync("https://ru.wikipedia.org/wiki/" + title + "?action=raw").Result;
                         string filename = file.Substring(5);
                         filename = "(" + Regex.Escape(filename) + "|" + Regex.Escape(Uri.EscapeDataString(filename)) + ")";
                         filename = filename.Replace(@"\ ", "[ _]");
@@ -58,15 +86,11 @@ class Program
                         text = r7.Replace(text, "");
                         text = r8.Replace(text, "$1");
                         text = r9.Replace(text, "$1");
-                        if (text != p.text)
+                        Save(site, "ru", title, text, "удаление несвободного файла из служебных пространств");
+                        if (r.GetAttribute("ns") == "10")
                         {
-                            site.PostDataAndGetResult("/w/api.php?action=edit&format=xml", "title=" + title + "&text=" + Uri.EscapeDataString(text) + "&summary=удаление несвободного файла из служебных пространств&token=" + token);
-                            if(r.GetAttribute("ns") == "10")
-                            {
-                                var track = new Page("u:MBH/Шаблоны с удалёнными файлами");
-                                track.Load();
-                                track.Save(track.text + "\n* [[" + title + "]]");
-                            }
+                            string tracktext = site.GetStringAsync("https://ru.wikipedia.org/wiki/u:MBH/Шаблоны с удалёнными файлами?action=raw").Result;
+                            Save(site, "ru", "u:MBH/Шаблоны с удалёнными файлами", tracktext + "\n* [[" + title + "]]", "");
                         }
                     }
                 }
