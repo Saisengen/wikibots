6054784e2b1dd9b17dd084a58ea7e5934ee0e7c6
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-06 14:54:43 +0700

    Update tagging-files-without-license.cs

--- other-bots/tagging-files-without-license.cs
+++ other-bots/tagging-files-without-license.cs
@@ -2,20 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.Xml;
 using System.IO;
-using DotNetWikiBot;
+using System.Net.Http;
+using System.Net;
 
 class Program
 {
+    static HttpClient Site(string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://ru.wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (result.ToString().Contains("uccess"))
+            Console.WriteLine(DateTime.Now.ToString() + " written " + title);
+        else
+            Console.WriteLine(result);
+    }
     static void Main()
     {
         var monthname = new string[13];
         monthname[1] = "января"; monthname[2] = "февраля"; monthname[3] = "марта"; monthname[4] = "апреля"; monthname[5] = "мая"; monthname[6] = "июня"; monthname[7] = "июля"; monthname[8] = "августа"; monthname[9] = "сентября"; monthname[10] = "октября"; monthname[11] = "ноября"; monthname[12] = "декабря";
         var autocatfiles = new HashSet<string>();
         var legalfiles = new HashSet<string>();
-        var creds = new StreamReader("p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
+        var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
+        var site = Site(creds[0], creds[1]);
         string cont, query, apiout;
-        apiout = site.GetWebPage("/w/api.php?action=query&list=categorymembers&format=xml&cmtitle=Категория:Файлы:Без машиночитаемой лицензии&cmprop=title&cmlimit=50");
+        apiout = site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&list=categorymembers&format=xml&cmtitle=Категория:Файлы:Без машиночитаемой лицензии&cmprop=title&cmlimit=50").Result;
         using (var r = new XmlTextReader(new StringReader(apiout)))
         {
             r.WhitespaceHandling = WhitespaceHandling.None;
@@ -23,10 +60,10 @@ class Program
                 if (r.NodeType == XmlNodeType.Element && r.Name == "cm")
                     autocatfiles.Add(r.GetAttribute("title"));
         }
-        cont = ""; query = "/w/api.php?action=query&list=embeddedin&format=xml&eititle=t:No license&einamespace=6&eilimit=max";
+        cont = ""; query = "https://ru.wikipedia.org/w/api.php?action=query&list=embeddedin&format=xml&eititle=t:No_license&einamespace=6&eilimit=max";
         while (cont != null)
         {
-            apiout = (cont == "" ? site.GetWebPage(query) : site.GetWebPage(query + "&eicontinue=" + Uri.EscapeDataString(cont)));
+            apiout = (cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&eicontinue=" + Uri.EscapeDataString(cont)).Result);
             using (var r = new XmlTextReader(new StringReader(apiout)))
             {
                 r.WhitespaceHandling = WhitespaceHandling.None;
@@ -37,16 +74,19 @@ class Program
             }
         }
 
+        bool atleast1file = false;
         autocatfiles.ExceptWith(legalfiles);
-        foreach (var n in autocatfiles)
+        foreach (var file in autocatfiles)
         {
-            var p = new Page(n);
-            p.Load();
-            p.Save("{{subst:nld}}\n" + p.text, "вынос на КБУ файла без валидной лицензии", false);
+            atleast1file = true;
+            string pagetext = site.GetStringAsync("https://ru.wikipedia.org/wiki/" + Uri.EscapeDataString(file) + "?action=raw").Result;
+            Save(site, file, "{{subst:nld}}\n" + pagetext, "вынос на КБУ файла без валидной лицензии");
         }
 
-        var dt = DateTime.Now;
-        var c = new Page("К:Файлы:Неясный лицензионный статус от " + dt.Day + " " + monthname[dt.Month] + " " + dt.Year);
-        c.Save("[[К:Файлы:Неясный лицензионный статус|" + dt.ToString("MM-dd") + "]]", "", false);
+        if (atleast1file)
+        {
+            var dt = DateTime.Now;
+            Save(site, "К:Файлы:Неясный лицензионный статус от " + dt.Day + " " + monthname[dt.Month] + " " + dt.Year, "[[К:Файлы:Неясный лицензионный статус|" + dt.ToString("MM-dd") + "]]", "вынос на КБУ файла без валидной лицензии");
+        }
     }
 }
