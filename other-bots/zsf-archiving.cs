2f8d27a8a5f7414d4a160e987e29d6e4cfb145a6
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-12 16:48:35 +0700

    Update zsf-archiving.cs

--- other-bots/zsf-archiving.cs
+++ other-bots/zsf-archiving.cs
@@ -1,21 +1,60 @@
 using System.IO;
-using DotNetWikiBot;
 using System.Text.RegularExpressions;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
+using System.Xml;
+using static System.Net.WebRequestMethods;
 
 class Program
 {
+    static HttpClient Site(string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://ru.wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (result.ToString().Contains("uccess"))
+            Console.WriteLine(DateTime.Now.ToString() + " written " + title);
+        else
+            Console.WriteLine(result);
+    }
     static void Main()
     {
         var year = DateTime.Now.Year;
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
-        var page = new Page("Википедия:Заявки на снятие флагов");
-        page.Load();
-        string zsftext = page.text;
+        var site = Site(creds[0], creds[1]);
+        string zsftext = site.GetStringAsync("https://ru.wikipedia.org/wiki/Википедия:Заявки на снятие флагов?action=raw").Result;
+        string initialtext = zsftext;
         var threadrgx = new Regex(@"\n\n==[^\n]*: флаг [^=]*==[^⇧]*===\s*Итог[^=]*===([^⇧]*)\((апат|пат|откат|загр|ПИ|ПФ|ПбП|инж|АИ|бот)\)\s*—\s*{{(за|против)([^⇧]*)⇧-->", RegexOptions.Singleline);
         var signature = new Regex(@"(\d\d:\d\d, \d{1,2} \w+ \d{4}) \(UTC\)");
-        var threads = threadrgx.Matches(page.text);
+        var threads = threadrgx.Matches(zsftext);
         foreach (Match thread in threads)
         {
             string archivepage = "";
@@ -61,11 +100,10 @@ class Program
                     continue;
             }
             zsftext = zsftext.Replace(threadtext, "");
-            var arch = new Page(archivepage);
-            arch.Load();
-            arch.Save(arch.text + threadtext, "", false);
+            string archivetext = site.GetStringAsync("https://ru.wikipedia.org/wiki/" + archivepage + "?action=raw").Result;
+            Save(site, archivepage, archivetext + threadtext, "");
         }
-        if (zsftext != page.text)
-            page.Save(zsftext, "архивация", false);
+        if (zsftext != initialtext)
+            Save(site, "Википедия:Заявки на снятие флагов", zsftext, "архивация");
     }
 }
