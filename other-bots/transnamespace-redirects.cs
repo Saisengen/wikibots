fefc82b372985e02977660dbb33d8a11d36d1168
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-13 17:29:52 +0700

    Update transnamespace-redirects.cs

--- other-bots/transnamespace-redirects.cs
+++ other-bots/transnamespace-redirects.cs
@@ -2,10 +2,48 @@ using System;
 using System.Collections.Generic;
 using System.Xml;
 using System.IO;
-using DotNetWikiBot;
+using System.Net.Http;
+using System.Net;
+using static System.Net.WebRequestMethods;
 
 class Program
 {
+    static HttpClient Site(string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://ru.wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (result.ToString().Contains("uccess"))
+            Console.WriteLine(DateTime.Now.ToString() + " written " + title);
+        else
+            Console.WriteLine(result);
+    }
     class redir
     {
         public string src, dest, srcns, destns;
@@ -19,10 +57,10 @@ class Program
     {
         var redirs = new Dictionary<string, redir>();
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
+        var site = Site(creds[0], creds[1]);
         var nss = new Dictionary<string, string>();
 
-        string apiout = site.GetWebPage("/w/api.php?action=query&meta=siteinfo&format=xml&siprop=namespaces");
+        string apiout = site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=siteinfo&format=xml&siprop=namespaces").Result;
         using (var r = new XmlTextReader(new StringReader(apiout)))
         {
             r.WhitespaceHandling = WhitespaceHandling.None;
@@ -38,10 +76,10 @@ class Program
 
         foreach (var n in nss)
         {
-            bool end = false; string cont = "", query = "/w/api.php?action=query&list=allredirects&format=xml&arprop=ids%7Ctitle&arnamespace=" + n.Key + "&arlimit=max";
+            bool end = false; string cont = "", query = "https://ru.wikipedia.org/w/api.php?action=query&list=allredirects&format=xml&arprop=ids%7Ctitle&arnamespace=" + n.Key + "&arlimit=max";
             while (end == false)
             {
-                apiout = (cont == "" ? site.GetWebPage(query) : site.GetWebPage(query + "&arcontinue=" + Uri.EscapeDataString(cont)));
+                apiout = (cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arcontinue=" + Uri.EscapeDataString(cont)).Result);
                 using (var rdr = new XmlTextReader(new StringReader(apiout)))
                 {
                     rdr.WhitespaceHandling = WhitespaceHandling.None;
@@ -70,7 +108,7 @@ class Program
 
         foreach(var q in requeststrings)
         {
-            string info = site.GetWebPage("/w/api.php?action=query&prop=info&format=xml&pageids=" + q);
+            string info = site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&prop=info&format=xml&pageids=" + q).Result;
             using (var rdr = new XmlTextReader(new StringReader(info)))
             {
                 rdr.WhitespaceHandling = WhitespaceHandling.None;
@@ -106,7 +144,6 @@ class Program
             }
             flag = false;
         }
-        var p = new Page("u:MBH/incorrect redirects");
-        p.Save(result, "", false);
+        Save(site, "u:MBH/incorrect redirects", result, "");
     }
 }
