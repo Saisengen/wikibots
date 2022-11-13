0f0789c934338873f0dfc5091740262693ebf035
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-14 04:27:23 +0700

    Update and rename trans-namespace-moves.cs to transnamespace-moves.cs

--- other-bots/transnamespace-moves.cs
+++ other-bots/transnamespace-moves.cs
@@ -2,20 +2,58 @@ using System;
 using System.Collections.Generic;
 using System.Xml;
 using System.IO;
-using DotNetWikiBot;
+using System.Net.Http;
+using System.Net;
+
 class record
 {
     public string oldtitle, oldns, newtitle, user, date, comment;
 }
 class Program
 {
+    static HttpClient Site(string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://ru.wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (result.ToString().Contains("uccess"))
+            Console.WriteLine(DateTime.Now.ToString() + " written " + title);
+        else
+            Console.WriteLine(result);
+    }
     static void Main()
     {
         var table = new List<record>();
         var apatusers = new HashSet<string>();
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
-        string apiout = site.GetWebPage("/w/api.php?action=query&list=logevents&format=xml&leprop=title%7Ctype%7Cuser%7Ctimestamp%7Ccomment%7Cdetails&letype=move&lelimit=max");
+        var site = Site(creds[0], creds[1]);
+        string apiout = site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&list=logevents&format=xml&leprop=title%7Ctype%7Cuser%7Ctimestamp%7Ccomment%7Cdetails&letype=move&lelimit=max").Result;
         using (var r = new XmlTextReader(new StringReader(apiout)))
         {
             r.WhitespaceHandling = WhitespaceHandling.None;
@@ -26,7 +64,7 @@ class Program
                     if (user == "Dibоt")
                         continue;
                     if (!apatusers.Contains(user))
-                        using (var rr = new XmlTextReader(new StringReader(site.GetWebPage("/w/api.php?action=query&format=xml&list=users&usprop=rights&ususers=" + user))))
+                        using (var rr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=users&usprop=rights&ususers=" + user).Result)))
                             while (rr.Read())
                                 if (rr.Value == "autoreview")
                                     apatusers.Add(user);
@@ -56,8 +94,6 @@ class Program
                 comment = t.comment;
             result += "\n|-" + (apatusers.Contains(t.user) ? "" : "style=\"background-color:#fcc\"") + "\n|" + t.date + "||[[:" + t.oldtitle + "|" + t.oldtitle + "]]||[[:" + t.newtitle + "]]||{{u|" + t.user + "}}||" + comment;
         }
-        result += "\n|}";
-        var p = new Page("Википедия:Страницы, перенесённые в пространство статей");
-        p.Save(result);
+        Save(site, "ВП:Страницы, перенесённые в пространство статей", result + "\n|}", "");
     }
 }
