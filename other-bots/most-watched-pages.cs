a517a6565707b0d0e82f7f05e0f6f70a95e42e40
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-14 05:52:20 +0700

    Update most-watched-pages.cs

--- other-bots/most-watched-pages.cs
+++ other-bots/most-watched-pages.cs
@@ -3,19 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using System.IO;
-using DotNetWikiBot;
 using System.Web.UI;
+using System.Net.Http;
+using System.Net;
 
 class Program
 {
+    static HttpClient Site(string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://ru.wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (result.ToString().Contains("uccess"))
+            Console.WriteLine(DateTime.Now.ToString() + " written " + title);
+        else
+            Console.WriteLine(result);
+    }
     static void Main()
     {
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
+        var site = Site(creds[0], creds[1]);
         var nss = new Dictionary<int, string>();
         string cont, query, apiout, result = "<center>\n";
 
-        apiout = site.GetWebPage("/w/api.php?action=query&meta=siteinfo&format=xml&siprop=namespaces");
+        apiout = site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=siteinfo&format=xml&siprop=namespaces").Result;
         using (var r = new XmlTextReader(new StringReader(apiout)))
         {
             r.WhitespaceHandling = WhitespaceHandling.None;
@@ -37,10 +74,10 @@ class Program
         {
             var pageids = new HashSet<string>();
             var pagecounts = new Dictionary<string, Pair>();
-            cont = ""; query = "/w/api.php?action=query&list=allpages&format=xml&aplimit=max&apfilterredir=nonredirects&apnamespace=";
+            cont = ""; query = "https://ru.wikipedia.org/w/api.php?action=query&list=allpages&format=xml&aplimit=max&apfilterredir=nonredirects&apnamespace=";
             while (cont != null)
             {
-                apiout = (cont == "" ? site.GetWebPage(query + n) : site.GetWebPage(query + n + "&apcontinue=" + Uri.EscapeDataString(cont)));
+                apiout = (cont == "" ? site.GetStringAsync(query + n).Result : site.GetStringAsync(query + n + "&apcontinue=" + Uri.EscapeDataString(cont)).Result);
                 using (var r = new XmlTextReader(new StringReader(apiout)))
                 {
                     r.WhitespaceHandling = WhitespaceHandling.None;
@@ -66,7 +103,7 @@ class Program
                 requeststrings.Add(idset.Substring(1));
 
             foreach (var q in requeststrings)
-                using (var r = new XmlTextReader(new StringReader(site.GetWebPage("/w/api.php?action=query&prop=info&format=xml&inprop=visitingwatchers%7Cwatchers&pageids=" + q))))
+                using (var r = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&prop=info&format=xml&inprop=visitingwatchers%7Cwatchers&pageids=" + q).Result)))
                 {
                     r.WhitespaceHandling = WhitespaceHandling.None;
                     while (r.Read())
@@ -80,16 +117,14 @@ class Program
                                 title = title.Replace("Обсуждение участника:", "Участник:").Replace("Обсуждение участницы:", "Участница:");
                             }
                             int watchers = Convert.ToInt16(r.GetAttribute("watchers"));
-                            string activewatchers;
-                            if (r.GetAttribute("visitingwatchers") != null)
-                                activewatchers = r.GetAttribute("visitingwatchers");
-                            else
-                                activewatchers = "";
+                            string activewatchers = r.GetAttribute("visitingwatchers");
+                            if (activewatchers == null)
+                                activewatchers = "<30";
                             if (n == 0 && watchers >= 50 || n != 0)
-                                pagecounts.Add(title, new Pair() { First = watchers, Second = activewatchers});
+                                pagecounts.Add(title, new Pair() { First = watchers, Second = activewatchers });
                         }
                 }
-            
+
             if (pagecounts.Count != 0)
             {
                 result += "==" + (nss[n] == "" ? "Статьи" : (nss[n] == "Обсуждение участника" ? "Участник" : nss[n])) + "==\n{|class=\"standard sortable\"\n!Страница!!Всего следящих!!Активных\n";
@@ -98,7 +133,6 @@ class Program
                 result += "|}\n";
             }
         }
-        var page = new DotNetWikiBot.Page("u:MBH/most watched pages");
-        page.Save(result);
+        Save(site, "u:MBH/most watched pages", result, "");
     }
 }
