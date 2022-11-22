269f3c2960acde29ae35961ee77a8bc31a0222b9
Saisengen <64943616+Saisengen@users.noreply.github.com>
2022-11-22 19:00:38 +0700

    Update extlink-domains-counter.cs

--- other-bots/extlink-domains-counter.cs
+++ other-bots/extlink-domains-counter.cs
@@ -3,47 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using System.IO;
-using DotNetWikiBot;
+using System.Net.Http;
+using System.Net;
 
 class Program
 {
+    static HttpClient Site(string login, string password)
+    {
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
+        client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://ru.wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        if (!result.IsSuccessStatusCode)
+            return null;
+        return client;
+    }
+    static void Save(HttpClient site, string title, string text, string comment)
+    {
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+        var request = new MultipartFormDataContent();
+        request.Add(new StringContent("edit"), "action");
+        request.Add(new StringContent(title), "title");
+        request.Add(new StringContent(text), "text");
+        request.Add(new StringContent(comment), "summary");
+        request.Add(new StringContent(token), "token");
+        request.Add(new StringContent("xml"), "format");
+        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (result.ToString().Contains("uccess"))
+            Console.WriteLine(DateTime.Now.ToString() + " written " + title);
+        else
+            Console.WriteLine(result);
+    }
     static void Main()
     {
         var links = new Dictionary<string, int>();
         var shortenedlinks = new Dictionary<string, int>();
-        var pages = new HashSet<string>();
-        var pagecounts = new Dictionary<string, int>();
-        var pagenames = new Dictionary<string, string>();
+        var pages = new Dictionary<string, string>();
         var requeststrings = new HashSet<string>();
         int c = 0; string testurl;
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        var site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
+        var site = Site(creds[0], creds[1]);
 
-        string cont = "", query = "/w/api.php?action=query&list=allpages&format=xml&apnamespace=0&apfilterredir=nonredirects&aplimit=max&rawcontinue=";
+        string cont = "", query = "https://ru.wikipedia.org/w/api.php?action=query&list=allpages&format=xml&apfilterredir=nonredirects&aplimit=max&rawcontinue=";
         while (cont != null)
         {
-            string apiout = (cont == "" ? site.GetWebPage(query) : site.GetWebPage(query + "&apcontinue=" + Uri.EscapeDataString(cont)));
+            string apiout = (cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&apcontinue=" + Uri.EscapeDataString(cont)).Result);
             using (var r = new XmlTextReader(new StringReader(apiout)))
             {
                 r.WhitespaceHandling = WhitespaceHandling.None;
                 r.Read(); r.Read(); r.Read(); r.Read(); cont = r.GetAttribute("apcontinue");
                 while (r.Read())
                     if (r.Name == "p")
-                    {
-                        string pid = r.GetAttribute("pageid");
-                        pages.Add(pid);
-                        pagenames.Add(pid, r.GetAttribute("title"));
-                        pagecounts.Add(pid, 0);
-                    }
+                        if (!pages.ContainsKey(r.GetAttribute("pageid")))
+                            pages.Add(r.GetAttribute("pageid"), r.GetAttribute("title"));
             }
         }
 
         string idset = "", id = "";
-        query = "/w/api.php?action=query&prop=extlinks&format=xml&ellimit=5000&pageids=";
-        foreach (var p in pages)
+        query = "https://ru.wikipedia.org/w/api.php?action=query&prop=extlinks&format=xml&ellimit=max&pageids=";
+        foreach (var p in pages.Keys)
         {
             idset += "|" + p;
-            if (++c % 500 == 0)
+            if (++c % 300 == 0)
             {
                 requeststrings.Add(idset.Substring(1));
                 idset = "";
@@ -52,11 +83,12 @@ class Program
         requeststrings.Add(idset.Substring(1));
 
         c = 0;
-        foreach (var q in requeststrings)
+        foreach (var reqstring in requeststrings)
         {
+            cont = "";
             while (cont != null)
             {
-                string apiout = (cont == "" ? site.GetWebPage(query + q) : site.GetWebPage(query + q + "&eloffset=" + cont));
+                string apiout = (cont == "" ? site.GetStringAsync(query + reqstring).Result : site.GetStringAsync(query + reqstring + "&eloffset=" + cont).Result);
                 using (var r = new XmlTextReader(new StringReader(apiout)))
                 {
                     r.WhitespaceHandling = WhitespaceHandling.None;
@@ -68,14 +100,13 @@ class Program
                         if (r.NodeType == XmlNodeType.Element && r.Name == "el")
                         {
                             r.Read();
-                            string str = r.Value;
-                            str = str.Substring(str.IndexOf("//") + 2);
-                            str = str.IndexOf("/") == -1 ? str : str.Substring(0, str.LastIndexOf("/"));
-                            if (!links.ContainsKey(str))
-                                links.Add(str, 1);
+                            string link = r.Value;
+                            link = link.Substring(link.IndexOf("//") + 2);
+                            link = link.IndexOf("/") == -1 ? link : link.Substring(0, link.LastIndexOf("/"));
+                            if (!links.ContainsKey(link))
+                                links.Add(link, 1);
                             else
-                                links[str]++;
-                            pagecounts[id]++;
+                                links[link]++;
                         }
                     }
                 }
@@ -90,24 +121,13 @@ class Program
             else
                 shortenedlinks.Add(l.Key, l.Value);
         }
-        //links = links.GroupBy(x => x.Key.StartsWith("www.") ? x.Key.Substring(4) : x.Key).ToDictionary(gr => gr.Select(x => x.Key).OrderByDescending(x => x.Length).First(), gr => gr.Sum(x => x.Value));
-
-        string result = "<center>\n{|class=\"standard\"\n!Место!!Число&nbsp;ссылок&nbsp;из&nbsp;рувики&nbsp;на!!style=\"text-align:left\"|данный сайт или его раздел";
+        string result = "\n{|class=\"standard\"\n!Место!!Число&nbsp;ссылок&nbsp;из&nbsp;рувики&nbsp;на!!style=\"text-align:left\"|данный сайт или его раздел";
         int counter = 0;
         foreach (var l in shortenedlinks.OrderByDescending(l => l.Value))
-            if (l.Value >= 70)
+            if (l.Value < 100)
+                break;
+            else
                 result += "\n|-\n|" + ++counter + "||" + l.Value + "||" + l.Key;
-        result += "|}";
-        var page = new Page("user:MBH/most linked sites");
-        page.Save(result);
-
-        //result = "<center>\n{|class=\"standard\"\n!Место!!Статья!!Внешних ссылок из неё";
-        //counter = 0;
-        //foreach (var p in pagecounts.OrderByDescending(p => p.Value))
-        //    if (p.Value >= 100)
-        //        result += "\n|-\n| " + ++counter + "||[[" + pagenames[p.Key] + "]]||" + p.Value;
-        //result += "|}";
-        //page = new Page("user:MBH/most extlinked pages");
-        //page.Save(result);
+        Save(site, "user:MBH/most linked sites", result + "\n|}", "");
     }
 }
