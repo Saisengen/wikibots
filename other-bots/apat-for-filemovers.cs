5ae1dacc2fd136fb9b94866ca99c6eb4016fe3fa
Saisengen <64943616+Saisengen@users.noreply.github.com>
2023-05-01 12:52:43 +0700

    Update apat-for-filemovers.cs

--- other-bots/apat-for-filemovers.cs
+++ other-bots/apat-for-filemovers.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Xml;
 using System.Net.Http;
 using System.Net;
+using System.Text.RegularExpressions;
 
 class Program
 {
@@ -78,8 +79,9 @@ class Program
 
         globalusers.ExceptWith(apats);
 
+        var lastmonth = DateTime.Now.AddMonths(-1);
         foreach (var mover in globalusers)
-            using (var rdr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=usercontribs&uclimit=max&ucend=" + (DateTime.Now.Year) + "-01-01T00:00:00.000Z&ucprop=comment&ucuser=" + Uri.EscapeDataString(mover)).Result)))
+            using (var rdr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=usercontribs&uclimit=max&ucend=" + lastmonth.ToString("yyyy-MM-dd") + "T00:00:00.000Z&ucprop=comment&ucuser=" + Uri.EscapeDataString(mover)).Result)))
                 while (rdr.Read())
                     if (rdr.Name == "item" && rdr.GetAttribute("comment") != null)
                         if (rdr.GetAttribute("comment").Contains("GR]"))
@@ -91,15 +93,15 @@ class Program
         if (globalusers_needs_flag.Count > 0)
         {
             string zkatext = site.GetStringAsync("https://ru.wikipedia.org/wiki/ВП:Запросы к администраторам?action=raw").Result;
-            string header = "{{/Шапка}}<!--Пожалуйста, не удаляйте эту строку, пишите ниже!-->\n";
-            string newmessage = "==Выдать апата глобальным правщикам==\nПеречисленные ниже участники занимаются переименованием файлов на Викискладе с заменой включений во всех разделах. В соответствии с [[ВП:ПАТ#ГЛОБ]] прошу выдать им апата, чтобы такие правки не распатрулировали страницы.";
+            var header = new Regex(@"(^\{[^\n]*\}\s*<[^>]*>\n)");
+            string newmessage = "==Выдать апата глобальным правщикам==\nПеречисленные ниже участники занимаются переименованием файлов на Викискладе с заменой включений во всех разделах. В соответствии с [[ВП:ПАТ#ГЛОБ]] прошу рассмотреть их вклад и выдать им апата, чтобы такие правки не распатрулировали страницы.";
             foreach (var mover in globalusers_needs_flag)
                 newmessage += "\n* [[special:contribs/" + mover + "|" + mover + "]]";
             newmessage += "\n~~~~\n\n";
-            if (zkatext.Contains(header))
-                Save(site, "ВП:Запросы к администраторам"/*"u:MBH/Черновик"*/, zkatext.Replace(header, header + "\n" + newmessage), "новые переименовывающие для выдачи апата");
+            if (header.IsMatch(zkatext))
+                Save(site, "ВП:Запросы к администраторам", header.Replace(zkatext, "$1" + "\n\n" + newmessage), "новые переименовывающие для выдачи апата");
             else
-                Save(site, "ВП:Запросы к администраторам"/*"u:MBH/Черновик"*/, newmessage + zkatext, "новые переименовывающие для выдачи апата");
+                Save(site, "ВП:Запросы к администраторам", newmessage + zkatext, "новые переименовывающие для выдачи апата");
         }
         
     }
