a4bc4aa90251d70d3727378e224bd74772dad012
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-02-18 19:11:00 +0700

    Update file-renaming.cs

--- files-management/file-renaming.cs
+++ files-management/file-renaming.cs
@@ -2,95 +2,98 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using DotNetWikiBot;
-
+using System.Threading;
 class MyBot : Bot
 {
     public static void Main()
     {
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
         Site ru = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
-        DateTime now = DateTime.UtcNow;
-        string log = "";
-
-        string xml = ru.GetWebPage(ru.apiPath + "?action=query&list=logevents&letype=move&lelimit=100&format=xml");
-        // RegExps
-        Regex items = new Regex("<item [^<]*?" + Regex.Escape("ns=\"6\"") + ".*?</item>", RegexOptions.Singleline);
-        Regex title = new Regex("(?<=title..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
-        Regex user = new Regex("(?<=user..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
-        Regex comment = new Regex("(?<=comment..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
-        Regex new_title = new Regex("(?<=target_title..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
-        int n = 0;
-        string[,] filemoves = new string[500, 4];
-        // Add data from RegExps to file-array 
-        foreach (Match m in items.Matches(xml))
+        while (true)
         {
-            filemoves[n, 0] = title.Matches(m.ToString())[0].ToString();
-            filemoves[n, 1] = user.Matches(m.ToString())[0].ToString();
-            filemoves[n, 2] = comment.Matches(m.ToString())[0].ToString();
-            filemoves[n, 3] = new_title.Matches(m.ToString())[0].ToString();
-            n++;
-        }
-        // look for usage of files
-        for (int i = 0; i < n; i++)
-        {
-            string[] filelinks = new string[500];
-            Regex iu = new Regex("<iu [^<]*? />", RegexOptions.Singleline);
-            int j = 0;
-            // add pages with filelinks to array
-            foreach (Match m in iu.Matches(ru.GetWebPage(ru.apiPath + "?action=query&list=imageusage&iutitle=" + Uri.EscapeUriString(filemoves[i, 0]) + "&format=xml")))
+            DateTime now = DateTime.UtcNow;
+            string log = "";
+            string xml = ru.GetWebPage(ru.apiPath + "?action=query&list=logevents&letype=move&lelimit=100&format=xml");
+            // RegExps
+            Regex items = new Regex("<item [^<]*?" + Regex.Escape("ns=\"6\"") + ".*?</item>", RegexOptions.Singleline);
+            Regex title = new Regex("(?<=title..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
+            Regex user = new Regex("(?<=user..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
+            Regex comment = new Regex("(?<=comment..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
+            Regex new_title = new Regex("(?<=target_title..).*?(?=" + Regex.Escape("\"") + ")", RegexOptions.Singleline);
+            int n = 0;
+            string[,] filemoves = new string[500, 4];
+            // Add data from RegExps to file-array 
+            foreach (Match m in items.Matches(xml))
             {
-                filelinks[j] = title.Matches(m.ToString())[0].ToString();
-                j++;
+                filemoves[n, 0] = title.Matches(m.ToString())[0].ToString();
+                filemoves[n, 1] = user.Matches(m.ToString())[0].ToString();
+                filemoves[n, 2] = comment.Matches(m.ToString())[0].ToString();
+                filemoves[n, 3] = new_title.Matches(m.ToString())[0].ToString();
+                n++;
             }
-            // replace moved files in pages
-            for (int k = 0; k < j; k++)
+            // look for usage of files
+            for (int i = 0; i < n; i++)
             {
-                bool done = false;
-                Page imagelink = new Page(ru, filelinks[k]);
-                imagelink.Load();
-                string filename = filemoves[i, 0].Replace("Файл:", "").Replace("&quot;", "\"").Replace("&amp;", "&").Replace("&#039;", "\'");
-                string filenewname = filemoves[i, 3];
-                // для Regex важно убрать плюсы и пробелы, а для вывода комментария в конце важно их оставить
-                string oldfilename = filename.Replace(" ", ".").Replace("+", ".").Replace("?", ".").Replace("*", ".").Replace("\\", ".").Replace("$", ".").Replace("(", ".").Replace(")", ".");
-                Regex fname = new Regex(@"(\n(\s)*|=(\s)*|:)" + oldfilename, RegexOptions.IgnoreCase | RegexOptions.Singleline);
-                foreach (Match m in fname.Matches(imagelink.text))
+                string[] filelinks = new string[500];
+                Regex iu = new Regex("<iu [^<]*? />", RegexOptions.Singleline);
+                int j = 0;
+                // add pages with filelinks to array
+                foreach (Match m in iu.Matches(ru.GetWebPage(ru.apiPath + "?action=query&list=imageusage&iutitle=" + Uri.EscapeUriString(filemoves[i, 0]) + "&format=xml")))
                 {
-                    if (imagelink.text.IndexOf(m.ToString()) == -1)
-                        done = false;
-
-                    filenewname = filemoves[i, 3].Replace("Файл:", "");
-                    for (int zxc = 0; zxc < 2; zxc++)
-                        filenewname = filenewname.Replace("&quot;", "\"").Replace("&#039;", "\'").Replace("&amp;", "&");
-                    if (m.ToString().IndexOf("=", 0, 2) != -1)
-                    {
-                        imagelink.text = imagelink.text.Replace(m.ToString(), "= " + filenewname);
-                        done = true;
-                    }
-                    else if (m.ToString().IndexOf(":", 0, 2) != -1)
-                    {
-                        imagelink.text = imagelink.text.Replace(m.ToString(), ":" + filenewname);
-                        done = true;
-                    }
-                    else if (m.ToString().IndexOf("\n", 0, 2) != -1)
-                    {
-                        imagelink.text = imagelink.text.Replace(m.ToString(), "\n" + filenewname);
-                        done = true;
-                    }
+                    filelinks[j] = title.Matches(m.ToString())[0].ToString();
+                    j++;
                 }
-                if (!done)
-                    log = log + "\n# [[:File:" + filenewname + "]] (" + now.ToString("dd MMMM yyyy") + ") - не удалось выполнить замену в [[" + imagelink.title + "]].";
-                string savecomment = "[[File:" + filename + "]] переименован [[User:" + filemoves[i, 1] + "|" + filemoves[i, 1] + "]] в [[File:" + filenewname + "]]";
-                if (filemoves[i, 2].Length > 0)
-                    savecomment = savecomment + " (" + filemoves[i, 2] + ")";
-                Console.WriteLine(savecomment);
-                imagelink.Save(savecomment, true);
+                // replace moved files in pages
+                for (int k = 0; k < j; k++)
+                {
+                    bool done = false;
+                    Page imagelink = new Page(ru, filelinks[k]);
+                    imagelink.Load();
+                    string filename = filemoves[i, 0].Replace("Файл:", "").Replace("&quot;", "\"").Replace("&amp;", "&").Replace("&#039;", "\'");
+                    string filenewname = filemoves[i, 3];
+                    // для Regex важно убрать плюсы и пробелы, а для вывода комментария в конце важно их оставить
+                    string oldfilename = filename.Replace(" ", ".").Replace("+", ".").Replace("?", ".").Replace("*", ".").Replace("\\", ".").Replace("$", ".").Replace("(", ".").Replace(")", ".");
+                    Regex fname = new Regex(@"(\n(\s)*|=(\s)*|:)" + oldfilename, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+                    foreach (Match m in fname.Matches(imagelink.text))
+                    {
+                        if (imagelink.text.IndexOf(m.ToString()) == -1)
+                            done = false;
+
+                        filenewname = filemoves[i, 3].Replace("Файл:", "");
+                        for (int zxc = 0; zxc < 2; zxc++)
+                            filenewname = filenewname.Replace("&quot;", "\"").Replace("&#039;", "\'").Replace("&amp;", "&");
+                        if (m.ToString().IndexOf("=", 0, 2) != -1)
+                        {
+                            imagelink.text = imagelink.text.Replace(m.ToString(), "= " + filenewname);
+                            done = true;
+                        }
+                        else if (m.ToString().IndexOf(":", 0, 2) != -1)
+                        {
+                            imagelink.text = imagelink.text.Replace(m.ToString(), ":" + filenewname);
+                            done = true;
+                        }
+                        else if (m.ToString().IndexOf("\n", 0, 2) != -1)
+                        {
+                            imagelink.text = imagelink.text.Replace(m.ToString(), "\n" + filenewname);
+                            done = true;
+                        }
+                    }
+                    if (!done)
+                        log = log + "\n# [[:File:" + filenewname + "]] (" + now.ToString("dd MMMM yyyy") + ") - не удалось выполнить замену в [[" + imagelink.title + "]].";
+                    string savecomment = "[[File:" + filename + "]] переименован [[User:" + filemoves[i, 1] + "|" + filemoves[i, 1] + "]] в [[File:" + filenewname + "]]";
+                    if (filemoves[i, 2].Length > 0)
+                        savecomment = savecomment + " (" + filemoves[i, 2] + ")";
+                    Console.WriteLine(savecomment);
+                    imagelink.Save(savecomment, true);
+                }
             }
-        }
-        if (log.Length > 2)
-        {
-            Page logpage = new Page(ru, "user:" + creds[0] + "/Переименования файлов");
-            logpage.Load();
-            logpage.Save(logpage.text += log, "ошибки обработки", false);
+            if (log.Length > 2)
+            {
+                Page logpage = new Page(ru, "user:" + creds[0] + "/Переименования файлов");
+                logpage.Load();
+                logpage.Save(logpage.text += log, "ошибки обработки", false);
+            }
+            Thread.Sleep(10000);
         }
     }
 }
