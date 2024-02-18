275c0ca3ada507b408250c921141e546fb6f24fe
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-02-18 08:48:23 +0700

    Update talk-redirs-delete.cs

--- other-bots/talk-redirs-delete.cs
+++ other-bots/talk-redirs-delete.cs
@@ -27,57 +27,60 @@ class Program
     {
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
         var site = Site(creds[8], creds[9]);
-        foreach (int ns in new int[] { 1,3,5,7,9,11,13,15,101,103,105,107,829 })
+        var doc = new XmlDocument();
+        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        if (!result.IsSuccessStatusCode)
+            return;
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
+
+        foreach (int ns in new int[] { 1,5,7,9,11,13,15,101,103,105,107,829 })
         {
-            using (var r = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=allredirects&arprop=title%7Cids&arnamespace=" + ns + "&arlimit=max").Result)))
-                while (r.Read())
-                    if (r.NodeType == XmlNodeType.Element && r.Name == "r")
-                    {
-                        string id = r.GetAttribute("fromid");
-                        using (var rr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&pageids=" + id + "&rvprop=ids&rvlimit=max").Result)))
+            string cont = "", query = "https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=allredirects&arprop=title%7Cids&arnamespace=" + ns + "&arlimit=max";
+            while (cont != null)
+            {
+                string apiout = (cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arcontinue=" + Uri.EscapeDataString(cont)).Result);
+                using (var r = new XmlTextReader(new StringReader(apiout)))
+                {
+                    r.WhitespaceHandling = WhitespaceHandling.None;
+                    r.Read(); r.Read(); cont = r.GetAttribute("arcontinue");
+                    while (r.Read())
+                        if (r.NodeType == XmlNodeType.Element && r.Name == "r")
                         {
-                            rr.WhitespaceHandling = WhitespaceHandling.None;
-                            while (rr.Read())
-                                if (rr.Name == "rev")
-                                {
-                                    rr.Read();
-                                    if (rr.NodeType == XmlNodeType.EndElement && rr.Name == "revisions")
-                                        using (var rrr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=backlinks&blpageid=" + id).Result)))
-                                        {
-                                            rrr.WhitespaceHandling = WhitespaceHandling.None;
-                                            bool there_are_links = false;
-                                            string title = "";
-                                            while (rrr.Read())
+                            string id = r.GetAttribute("fromid");
+                            using (var rr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&pageids=" + id + "&rvprop=ids&rvlimit=max").Result)))
+                            {
+                                rr.WhitespaceHandling = WhitespaceHandling.None;
+                                while (rr.Read())
+                                    if (rr.Name == "rev")
+                                    {
+                                        rr.Read();
+                                        if (rr.NodeType == XmlNodeType.EndElement && rr.Name == "revisions")
+                                            using (var rrr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=backlinks&blpageid=" + id).Result)))
                                             {
-                                                if (rrr.GetAttribute("title") != null)
-                                                    title = rrr.GetAttribute("title");
-                                                if (rrr.Name == "bl" && !title.StartsWith("Википедия:Страницы с похожими названиями") && !title.StartsWith("Участник:DvoreBot/Оставленные перенаправления"))
-                                                    there_are_links = true;
+                                                rrr.WhitespaceHandling = WhitespaceHandling.None;
+                                                bool there_are_links = false;
+                                                while (rrr.Read())
+                                                    if (rrr.Name == "bl" && !rrr.GetAttribute("title").StartsWith("Википедия:Страницы с похожими названиями") && !rrr.GetAttribute("title").StartsWith("Участник:DvoreBot/Оставленные перенаправления"))
+                                                        there_are_links = true;
+                                                if (!there_are_links)
+                                                {
+                                                    var request = new MultipartFormDataContent();
+                                                    request.Add(new StringContent("delete"), "action");
+                                                    request.Add(new StringContent(id), "pageid");
+                                                    request.Add(new StringContent("[[ВП:КБУ#П6|редирект между СО без ссылок]]"), "reason");
+                                                    request.Add(new StringContent(token), "token");
+                                                    result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+                                                    if (!result.ToString().Contains("uccess"))
+                                                        Console.WriteLine(result);
+                                                }
                                             }
-                                            if (!there_are_links)
-                                            {
-                                                var doc = new XmlDocument();
-                                                var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
-                                                if (!result.IsSuccessStatusCode)
-                                                    return;
-                                                doc.LoadXml(result.Content.ReadAsStringAsync().Result);
-                                                var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
-                                                var request = new MultipartFormDataContent();
-                                                request.Add(new StringContent("delete"), "action");
-                                                request.Add(new StringContent(id), "pageid");
-                                                request.Add(new StringContent("[[ВП:КБУ#П6]]"), "reason");
-                                                request.Add(new StringContent(token), "token");
-                                                result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
-                                                if (result.ToString().Contains("uccess"))
-                                                    Console.WriteLine("deleted " + title);
-                                                else
-                                                    Console.WriteLine(result);
-                                            }
-                                        }
-                                    break;
-                                }
+                                        break;
+                                    }
+                            }
                         }
-                    }
+                }
+            }
         }
     }
 }
