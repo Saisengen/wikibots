3b1e37dd458df45e47c2551ebbe9e34b2fc223ea
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-02-23 14:25:47 +0700

    Update and rename talk-redirs-delete.cs to talk-and-broken-redirs-delete.cs

--- other-bots/talk-and-broken-redirs-delete.cs
+++ other-bots/talk-and-broken-redirs-delete.cs
@@ -40,9 +40,9 @@ class Program
                 if (r.Name == "cm")
                     legal_redirs.Add(r.GetAttribute("pageid"));
         
-        foreach (int ns in new int[] { 1,5,7,9,11,13,15,101,103,105,107,829 })
+        foreach (int ns in new int[] { 1,3,5,7,9,11,13,15,101,103,105,107,829 })
         {
-            string cont = "", query = "https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=allredirects&arprop=title%7Cids&arnamespace=" + ns + "&arlimit=max";
+            string cont = "", query = "https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=allredirects&arprop=title|ids&arnamespace=" + ns + "&arlimit=max";
             while (cont != null)
             {
                 string apiout = (cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arcontinue=" + Uri.EscapeDataString(cont)).Result);
@@ -51,10 +51,15 @@ class Program
                     r.WhitespaceHandling = WhitespaceHandling.None;
                     r.Read(); r.Read(); cont = r.GetAttribute("arcontinue");
                     while (r.Read())
-                        if (r.NodeType == XmlNodeType.Element && r.Name == "r")
+                        if (r.Name == "r" && (ns != 3 || r.GetAttribute("title").Contains("/")))
                         {
+                            int cntr = 0;
                             string id = r.GetAttribute("fromid");
-                            if (!legal_redirs.Contains(id))
+                            using (var rr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&pageids=" + id + "&rvprop=ids&rvlimit=max").Result)))
+                                while (rr.Read())
+                                    if (rr.Name == "rev" && rr.NodeType == XmlNodeType.Element)
+                                        cntr++;
+                            if (!legal_redirs.Contains(id) && cntr == 1)
                             {
                                 using (var rr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&pageids=" + id + "&rvprop=ids&rvlimit=max").Result)))
                                 {
@@ -88,8 +93,35 @@ class Program
                                 }
                             }
                         }
+
                 }
             }
         }
+
+        using (var r = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&list=querypage&qppage=BrokenRedirects&qplimit=max").Result)))
+            while (r.Read())
+                if (r.Name == "page")
+                {
+                    string title = r.GetAttribute("title");
+                    string ns = r.GetAttribute("ns");
+                    if (ns != "2" || (ns == "2" && title.Contains("/")))
+                        using (var rr = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&titles=" + title + "&rvprop=ids&rvlimit=max").Result)))
+                        {
+                            int cntr = 0;
+                            while (rr.Read())
+                                if (rr.Name == "rev" && rr.NodeType == XmlNodeType.Element)
+                                    cntr++;
+                            if (cntr == 1)
+                            {
+                                var request = new MultipartFormDataContent();
+                                request.Add(new StringContent("delete"), "action");
+                                request.Add(new StringContent(title), "title");
+                                request.Add(new StringContent(token), "token");
+                                result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+                                if (!result.ToString().Contains("uccess"))
+                                    Console.WriteLine(result);
+                            }
+                        }
+                }
     }
 }
