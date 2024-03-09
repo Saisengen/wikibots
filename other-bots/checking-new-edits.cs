5471cb954d54dc2c773e7349faa95c7c5a4f8da3
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-03-09 18:31:30 +0700

    Update checking-new-edits.cs

--- other-bots/checking-new-edits.cs
+++ other-bots/checking-new-edits.cs
@@ -16,6 +16,7 @@ class Program
     static Regex rowrx = new Regex(@"\|-");
     static Dictionary<string,string> notifying_page_name = new Dictionary<string,string>(){{"ru","Рейму_Хакурей/Проблемные_правки"},{"uk","Рейму_Хакурей/Підозрілі_редагування"}};
     static Dictionary<string,string> notifying_header = new Dictionary<string, string>() { { "ru", "!Дифф!!Статья!!Автор!!Причина" }, { "uk", "!Diff!!Стаття!!Автор!!Причина" } };
+    static Dictionary<string, bool> users_by_apat_flag = new Dictionary<string, bool>();
     enum edit_type { zkab_report, talkpage_warning, suspicious_edit, rollback }
     static HttpClient Site(string lang, string login, string password)
     {
@@ -26,7 +27,8 @@ class Program
         var doc = new XmlDocument();
         doc.LoadXml(result.Content.ReadAsStringAsync().Result);
         var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
-        result = client.PostAsync("https://" + lang + ".wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
+        result = client.PostAsync("https://" + lang + ".wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login },
+            { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
         if (!result.IsSuccessStatusCode)
             return null;
         return client;
@@ -71,6 +73,22 @@ class Program
         discord.PostAsync("https://discord.com/api/webhooks/" + discord_token, new FormUrlEncodedContent(new Dictionary<string, string>{ { "content", "[" + title + "](<https://" + lang +
         ".wikipedia.org/w/index.php?diff=" + newid + ">) / [" + user.Replace(' ', '_') + "](<https://" + lang + ".wikipedia.org/wiki/special:contribs/" + user.Replace(' ', '_') + ">) " + reason}}));
     }
+    static bool isApat(string user)
+    {
+        if (users_by_apat_flag.ContainsKey(user))
+            return users_by_apat_flag[user];
+        else
+            if (ru.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&format=json&list=users&usprop=rights&ususers=" + Uri.EscapeDataString(user)).Result.Contains("review"))
+        {
+            users_by_apat_flag.Add(user, true);
+            return true;
+        }
+        else
+        {
+            users_by_apat_flag.Add(user, false);
+            return false;
+        }
+    }
     static int Main()
     {
         var goodanons = new HashSet<string>();
@@ -81,7 +99,7 @@ class Program
         ru = Site("ru", creds[4], creds[5]);
         uk = Site("uk", creds[4], creds[5]);
         var reportedusersrx = new Regex(@"\| вопрос = u/(.*)");
-        string rulasteditid = "", newrulasteditid = "", ukrlasteditid = "", newukrlasteditid = "";
+        string rulasteditid = "", newrulasteditid = "", ukrlasteditid = "", newukrlasteditid = "", commandtext;
         double mediumlimit = 1, lowlimit = 1, ukrlimit = 1;
         int currminute = -1;
         var badusers = new HashSet<string>();
@@ -89,124 +107,118 @@ class Program
         ruconnect.Open();
         var ukrconnect = new MySqlConnection(creds[2].Replace("%project%", "ukwiki").Replace("analytics", "web"));
         ukrconnect.Open();
-        MySqlDataReader rureader, ukrreader;//попытка решить проблему, что ру-правки постятся в укрвики
+        MySqlDataReader sqlreader;
         string diff_text;
         while (true)
         {
-            try
+            if (currminute != DateTime.UtcNow.Minute / 10)
             {
-                if (currminute != DateTime.UtcNow.Minute / 10)
+                currminute = DateTime.UtcNow.Minute / 10;
+                var limits = ru.GetStringAsync("https://ru.wikipedia.org/w/index.php?title=user:MBH/limits.css&action=raw").Result.Split('\n');
+                lowlimit = Convert.ToDouble(limits[0]);
+                mediumlimit = Convert.ToDouble(limits[1]);
+                ukrlimit = Convert.ToDouble(limits[2]);
+                foreach (var g in ru.GetStringAsync("https://ru.wikipedia.org/w/index.php?title=user:MBH/goodanons.css&action=raw").Result.Split('\n'))
+                    goodanons.Add(g);
+                patterns.Clear();
+                patterns.Add(new Regex(@"\bСВО\b")); //нельзя использовать в ignore case, как остальные
+                patterns.Add(new Regex(@"[хxX][oaо0аАОAO][XХxх][лLлl]\w*")); //исключаем фамилию Хохлов
+                var pattern_source = new StreamReader("patterns.txt").ReadToEnd().Split('\n');
+                foreach (var pattern in pattern_source)
+                    patterns.Add(new Regex(pattern, RegexOptions.IgnoreCase));
+            }
+            bool runewle = false, ukrnewle = false;
+            commandtext = "select actor_user, cast(rc_title as char) title, oresc_probability, cast(actor_name as char) user, rc_this_oldid, rc_last_oldid from recentchanges join " +
+                "ores_classification on oresc_rev=rc_this_oldid join actor on actor_id=rc_actor join ores_model on oresc_model=oresm_id where rc_timestamp>" + DateTime.UtcNow.AddSeconds(-10)
+                .ToString("yyyyMMddHHmmss") + " and (rc_type=0 or rc_type=1) and rc_namespace=0 and oresm_name=\"damaging\" order by rc_this_oldid desc;";
+
+            sqlreader = new MySqlCommand(commandtext, ruconnect).ExecuteReader();
+            while (sqlreader.Read())
+            {
+                user = sqlreader.GetString("user") ?? "";
+                if (goodanons.Contains(user))
+                    continue;
+                bool user_is_anon = sqlreader.IsDBNull(0);
+                damaging = Math.Round(sqlreader.GetDouble("oresc_probability"), 3);
+                title = sqlreader.GetString("title").Replace('_', ' ');
+                newid = sqlreader.GetString("rc_this_oldid");
+                oldid = sqlreader.GetString("rc_last_oldid");
+                if (!runewle)
                 {
-                    currminute = DateTime.UtcNow.Minute / 10;
-                    var limits = ru.GetStringAsync("https://ru.wikipedia.org/w/index.php?title=user:MBH/limits.css&action=raw").Result.Split('\n');
-                    lowlimit = Convert.ToDouble(limits[0]);
-                    mediumlimit = Convert.ToDouble(limits[1]);
-                    ukrlimit = Convert.ToDouble(limits[2]);
-                    foreach (var g in ru.GetStringAsync("https://ru.wikipedia.org/w/index.php?title=user:MBH/goodanons.css&action=raw").Result.Split('\n'))
-                        goodanons.Add(g);
-                    patterns.Clear();
-                    patterns.Add(new Regex(@"\bСВО\b")); //нельзя использовать в ignore case, как остальные
-                    patterns.Add(new Regex(@"[хxX][oaо0аАОAO][XХxх][лLлl]\w*")); //исключаем фамилию Хохлов
-                    var pattern_source = new StreamReader("patterns.txt").ReadToEnd().Split('\n');
-                    foreach (var pattern in pattern_source)
-                        patterns.Add(new Regex(pattern, RegexOptions.IgnoreCase));
+                    newrulasteditid = newid;
+                    runewle = true;
                 }
-                bool runewle = false, ukrnewle = false;
-                string commandtext = "select actor_user, cast(rc_title as char) title, oresc_probability, cast(actor_name as char) user, rc_this_oldid, rc_last_oldid from recentchanges join " +
-                    "ores_classification on oresc_rev=rc_this_oldid join actor on actor_id=rc_actor join ores_model on oresc_model=oresm_id where rc_timestamp>" + DateTime.UtcNow.AddSeconds(-10)
-                    .ToString("yyyyMMddHHmmss") + " and (rc_type=0 or rc_type=1) and rc_namespace=0 and oresm_name=\"damaging\" order by rc_this_oldid desc;";
-                rureader = new MySqlCommand(commandtext, ruconnect).ExecuteReader();
-                while (rureader.Read())
+                if (newid == rulasteditid)
+                    break;
+
+                if (damaging > lowlimit)
                 {
-                    user = rureader.GetString("user") ?? "";
-                    if (goodanons.Contains(user))
-                        continue;
-                    bool user_is_anon = rureader.IsDBNull(0);
-                    damaging = Math.Round(rureader.GetDouble("oresc_probability"), 3);
-                    title = rureader.GetString("title").Replace('_', ' ');
-                    newid = rureader.GetString("rc_this_oldid");
-                    oldid = rureader.GetString("rc_last_oldid");
-                    if (!runewle)
+                    if (badusers.Contains(user))
                     {
-                        newrulasteditid = newid;
-                        runewle = true;
+                        string zkab = ru.GetStringAsync("https://ru.wikipedia.org/w/index.php?title=ВП:Запросы_к_администраторам/Быстрые&action=raw").Result;
+                        var reportedusers = reportedusersrx.Matches(zkab);
+                        bool reportedyet = false;
+                        foreach (Match r in reportedusers)
+                            if (user == r.Groups[1].Value)
+                                reportedyet = true;
+                        if (!reportedyet)
+                            Save("ru", ru, "edit", "ВП:Запросы к администраторам/Быстрые", "\n\n{{subst:t:preload/ЗКАБ/subst|участник=" + user + "|пояснение=}}", "[[special:contribs/" + user +
+                                "]] - новый запрос", edit_type.zkab_report);
                     }
-                    if (newid == rulasteditid)
-                        break;
+                    else badusers.Add(user);
 
-                    if (damaging > lowlimit)
+                    if (damaging < mediumlimit)
+                        post_suspicious_edit("ru", damaging.ToString());
+
+                    else
                     {
-                        if (badusers.Contains(user))
-                        {
-                            string zkab = ru.GetStringAsync("https://ru.wikipedia.org/w/index.php?title=ВП:Запросы_к_администраторам/Быстрые&action=raw").Result;
-                            var reportedusers = reportedusersrx.Matches(zkab);
-                            bool reportedyet = false;
-                            foreach (Match r in reportedusers)
-                                if (user == r.Groups[1].Value)
-                                    reportedyet = true;
-                            if (!reportedyet)
-                                Save("ru", ru, "edit", "ВП:Запросы к администраторам/Быстрые", "\n\n{{subst:t:preload/ЗКАБ/subst|участник=" + user + "|пояснение=}}", "[[special:contribs/" + user +
-                                    "]] - новый запрос", edit_type.zkab_report);
-                        }
-                        else badusers.Add(user);
-
-                        if (damaging < mediumlimit)
-                            post_suspicious_edit("ru", damaging.ToString());
-
+                        string answer = Save("ru", ru, "rollback", title, user, "[[u:Рейму Хакурей|автоматическая отмена]] правки участника [[special:contribs/" + user + "|" + user + "]] (" +
+                            damaging + ")", edit_type.rollback);
+                        if (answer.Contains("<rollback title="))
+                            Save("ru", ru, "edit", "ut:" + user, "{{subst:u:Рейму_Хакурей/Уведомление|" + newid + "|" + title + "|" + damaging + "|" + (user_is_anon ? "1" : "") +
+                                "}}", (user_is_anon ? "Правка с вашего IP-адреса" : "Ваша правка") + " в статье [[" + title + "]] " + "автоматически отменена", edit_type.talkpage_warning);
                         else
-                        {
-                            string answer = Save("ru", ru, "rollback", title, user, "[[u:Рейму Хакурей|автоматическая отмена]] правки участника [[special:contribs/" + user + "|" + user + "]] (" +
-                                damaging + ")", edit_type.rollback);
-                            if (answer.Contains("<rollback title="))
-                                Save("ru", ru, "edit", "ut:" + user, "{{subst:u:Рейму_Хакурей/Уведомление|" + newid + "|" + title + "|" + damaging + "|" + (user_is_anon ? "1" : "") +
-                                    "}}", (user_is_anon ? "Правка с вашего IP-адреса" : "Ваша правка") + " в статье [[" + title + "]] " + "автоматически отменена", edit_type.talkpage_warning);
-                            else
-                                Console.WriteLine(title + "\n" + answer);
-                        }
-                    }
-
-                    if (user_is_anon)
-                    {
-                        try { diff_text = ru.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=compare&format=xml&fromrev=" + oldid + "&torev=" + newid + "&prop=diff&difftype=unified").Result; }
-                        catch { continue; }
-                        foreach (Match added_string in added_string_rgx.Matches(diff_text))
-                            foreach (var pattern in patterns)
-                                if (pattern.IsMatch(added_string.Value))
-                                {
-                                    post_suspicious_edit("ru", pattern.Match(added_string.Value).Value);
-                                    break;
-                                }
+                            Console.WriteLine(title + "\n" + answer);
                     }
                 }
-                rureader.Close();
-                rulasteditid = newrulasteditid;
 
-                ukrreader = new MySqlCommand(commandtext, ukrconnect).ExecuteReader();
-                while (ukrreader.Read())
+                if (user_is_anon || !isApat(user))
                 {
-                    string user = ukrreader.GetString("user");
-                    double damaging = Math.Round(ukrreader.GetDouble("oresc_probability"), 3);
-                    string title = ukrreader.GetString("title").Replace('_', ' ');
-                    string editid = ukrreader.GetString("rc_this_oldid");
-                    if (!ukrnewle)
-                    {
-                        newrulasteditid = editid;
-                        ukrnewle = true;
-                    }
-                    if (editid == ukrlasteditid)
-                        break;
-
-                    if (damaging > ukrlimit)
-                        post_suspicious_edit("uk", damaging.ToString());
+                    try { diff_text = ru.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=compare&format=xml&fromrev=" + oldid + "&torev=" + newid + "&prop=diff&difftype=unified").Result; }
+                    catch { continue; }
+                    foreach (Match added_string in added_string_rgx.Matches(diff_text))
+                        foreach (var pattern in patterns)
+                            if (pattern.IsMatch(added_string.Value))
+                            {
+                                post_suspicious_edit("ru", pattern.Match(added_string.Value).Value);
+                                break;
+                            }
                 }
-                ukrreader.Close();
-                ukrlasteditid = newukrlasteditid;
             }
-            catch (Exception e)
+            sqlreader.Close();
+            rulasteditid = newrulasteditid;
+
+            sqlreader = new MySqlCommand(commandtext, ukrconnect).ExecuteReader();
+            while (sqlreader.Read())
             {
-                Console.WriteLine(e.ToString());
-                return 1;
+                user = sqlreader.GetString("user");
+                damaging = Math.Round(sqlreader.GetDouble("oresc_probability"), 3);
+                title = sqlreader.GetString("title").Replace('_', ' ');
+                newid = sqlreader.GetString("rc_this_oldid");
+                if (!ukrnewle)
+                {
+                    newrulasteditid = newid;
+                    ukrnewle = true;
+                }
+                if (newid == ukrlasteditid)
+                    break;
+
+                if (damaging > ukrlimit)
+                    post_suspicious_edit("uk", damaging.ToString());
             }
+            sqlreader.Close();
+            ukrlasteditid = newukrlasteditid;
+
             Thread.Sleep(5000);
         }
     }
