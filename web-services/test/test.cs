ab812209f141a7dbad68a25aef42b6e65bf36ec3
David Caro <me@dcaro.es>
2024-03-12 10:27:43 +0100

    test: Don't print passwords
    Signed-off-by: David Caro <me@dcaro.es>

--- web-services/test/test.cs
+++ web-services/test/test.cs
@@ -6,6 +6,11 @@ internal class Program
         Console.WriteLine("Content-type: text/html\n");
         var a = Environment.GetEnvironmentVariables();
         foreach (var v in a.Keys)
+        {
+            if (v.ToString().Contains("PASS") || v.ToString().Contains("CRED"))
+                continue;
+
             Console.WriteLine(v.ToString() + " = " + Environment.GetEnvironmentVariable(v.ToString()));
+        }
     }
 }
