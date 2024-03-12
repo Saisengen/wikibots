7e29afa8d9e39eae318f98ede159598a267912c8
David Caro <me@dcaro.es>
2024-03-12 10:37:35 +0100

    test: use an html table
    Signed-off-by: David Caro <me@dcaro.es>

--- web-services/test/test.cs
+++ web-services/test/test.cs
@@ -5,12 +5,14 @@ internal class Program
     {
         Console.WriteLine("Content-type: text/html\n");
         var a = Environment.GetEnvironmentVariables();
+        Console.WriteLine("<body><table><tr><th>Env var</th><th>Value</th><tr>");
         foreach (var v in a.Keys)
         {
             if (v.ToString().Contains("PASS") || v.ToString().Contains("CRED"))
                 continue;
 
-            Console.WriteLine(v.ToString() + " = " + Environment.GetEnvironmentVariable(v.ToString()));
+            Console.WriteLine("<tr><td>" + v.ToString() + "</td><td>" + Environment.GetEnvironmentVariable(v.ToString()) + "</td></tr>\n");
         }
+        Console.WriteLine("</table></body>");
     }
 }
