1c5acb5d10a3390c30b156754526c002b7f43f50
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-03-12 21:16:51 +0700

    Update test.cs
    can't sort allvars for unknown reasons, so copied it to another dict and sorted it

--- web-services/test/test.cs
+++ web-services/test/test.cs
@@ -1,18 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 internal class Program
 {
     static void Main()
     {
         Console.WriteLine("Content-type: text/html\n");
-        var a = Environment.GetEnvironmentVariables();
+        var allvars = Environment.GetEnvironmentVariables();
+        var dict = new Dictionary<string, string>();
         Console.WriteLine("<body><table><tr><th>Env var</th><th>Value</th><tr>");
-        foreach (var v in a.Keys)
-        {
-            if (v.ToString().Contains("PASS") || v.ToString().Contains("CRED"))
-                continue;
+        foreach (var v in allvars.Keys)
+            dict.Add(v.ToString(), allvars[v].ToString());
 
-            Console.WriteLine("<tr><td>" + v.ToString() + "</td><td>" + Environment.GetEnvironmentVariable(v.ToString()) + "</td></tr>\n");
-        }
+        foreach (var key in dict.Keys.OrderBy(d => d))
+            Console.WriteLine("<tr><td>" + key + "</td><td>" + ((key.Contains("PASS") || key.Contains("CRED")) ? "" : dict[key]) + "</td></tr>\n");
         Console.WriteLine("</table></body>");
     }
 }
