370ec83af3ef7f7fdceb783dd240856b8b8da4f7
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-03-14 19:55:31 +0700

    Update test.cs

--- web-services/test/test.cs
+++ web-services/test/test.cs
@@ -8,12 +8,12 @@ internal class Program
         Console.WriteLine("Content-type: text/html\n");
         var allvars = Environment.GetEnvironmentVariables();
         var dict = new Dictionary<string, string>();
-        Console.WriteLine("<body><table><tr><th>Env var</th><th>Value</th><tr>");
+        Console.WriteLine("<body><table border=1><tr><th>Env var</th><th>Value</th><tr>");
         foreach (var v in allvars.Keys)
             dict.Add(v.ToString(), allvars[v].ToString());
 
         foreach (var key in dict.Keys.OrderBy(d => d))
-            Console.WriteLine("<tr><td>" + key + "</td><td>" + ((key.Contains("PASS") || key.Contains("CRED")) ? "" : dict[key]) + "</td></tr>\n");
+            Console.WriteLine("<tr><td>" + key + "</td><td>" + ((key.Contains("PASS") || key == "CONN_STRING") ? "" : dict[key]) + "</td></tr>\n");
         Console.WriteLine("</table></body>");
     }
 }
