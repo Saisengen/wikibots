7cd091d08a09b306aed8b7717239b312f7fcb324
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-10-15 00:36:14 +0700

    Update popular_userscripts.cs

--- other-bots/popular_userscripts.cs
+++ other-bots/popular_userscripts.cs
@@ -17,18 +17,18 @@ class Program
     {
         var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
         client.DefaultRequestHeaders.Add("User-Agent", login);
-        var result = client.GetAsync("https://" + g_lang + ".wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
+        var result = client.GetAsync("https://" + lang + ".wikipedia.org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
         var doc = new XmlDocument();
         doc.LoadXml(result.Content.ReadAsStringAsync().Result);
         var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
-        client.PostAsync("https://" + g_lang + ".wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password },
+        client.PostAsync("https://" + lang + ".wikipedia.org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password },
             { "lgtoken", logintoken }, { "format", "xml" } }));
         return client;
     }
     static void Save(HttpClient site, string title, string text, string comment)
     {
         var doc = new XmlDocument();
-        var result = site.GetAsync("https://" + g_lang + ".wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
+        var result = site.GetAsync("https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
         if (!result.IsSuccessStatusCode)
             return;
         doc.LoadXml(result.Content.ReadAsStringAsync().Result);
@@ -40,27 +40,27 @@ class Program
         request.Add(new StringContent(comment), "summary");
         request.Add(new StringContent(token), "token");
         request.Add(new StringContent("xml"), "format");
-        result = site.PostAsync("https://" + g_lang + ".wikipedia.org/w/api.php", request).Result;
+        result = site.PostAsync("https://" + lang + ".wikipedia.org/w/api.php", request).Result;
         if (result.ToString().Contains("uccess"))
-            Console.WriteLine(DateTime.Now.ToString() + " written " + g_lang + ":" + title);
+            Console.WriteLine(DateTime.Now.ToString() + " written " + lang + ":" + title);
         else
             Console.WriteLine(result);
     }
     static bool user_is_active()
     {
-        if (users_activity[g_lang].ContainsKey(g_username))
-            return users_activity[g_lang][g_username];
+        if (users_activity[lang].ContainsKey(username))
+            return users_activity[lang][username];
         else
         {
             DateTime edit_ts = new DateTime(), log_ts = new DateTime();
-            using (var r = new XmlTextReader(new StringReader(site[g_lang].GetStringAsync("https://" + g_lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=usercontribs&uclimit=1&ucprop=timestamp&ucuser=" + Uri.EscapeUriString(g_username)).Result)))
+            using (var r = new XmlTextReader(new StringReader(site[lang].GetStringAsync("https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=usercontribs&uclimit=1&ucprop=timestamp&ucuser=" + Uri.EscapeUriString(username)).Result)))
                 while (r.Read())
                     if (r.Name == "item")
                     {
                         string raw_ts = r.GetAttribute("timestamp");
                         edit_ts = new DateTime(Convert.ToInt16(raw_ts.Substring(0, 4)), Convert.ToInt16(raw_ts.Substring(5, 2)), Convert.ToInt16(raw_ts.Substring(8, 2)));
                     }
-            using (var r = new XmlTextReader(new StringReader(site[g_lang].GetStringAsync("https://" + g_lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=logevents&leprop=timestamp&lelimit=1&leuser=" + Uri.EscapeUriString(g_username)).Result)))
+            using (var r = new XmlTextReader(new StringReader(site[lang].GetStringAsync("https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=logevents&leprop=timestamp&lelimit=1&leuser=" + Uri.EscapeUriString(username)).Result)))
                 while (r.Read())
                     if (r.Name == "item")
                     {
@@ -70,11 +70,11 @@ class Program
 
             if (edit_ts < DateTime.Now.AddMonths(-1) && log_ts < DateTime.Now.AddMonths(-1))
             {
-                users_activity[g_lang].Add(g_username, false); return false;
+                users_activity[lang].Add(username, false); return false;
             }
             else
             {
-                users_activity[g_lang].Add(g_username, true); return true;
+                users_activity[lang].Add(username, true); return true;
             }
 
         }
@@ -83,7 +83,7 @@ class Program
     {
         if (scriptname.StartsWith(":"))
             scriptname = scriptname.Substring(1);
-        if (scriptname.StartsWith(g_lang + ":"))
+        if (scriptname.StartsWith(lang + ":"))
             scriptname = scriptname.Substring(3);
         if (scriptname.IndexOf(":") > -1)
             scriptname = scriptname.Substring(0, scriptname.IndexOf(":")).ToLower() + scriptname.Substring(scriptname.IndexOf(":"));
@@ -93,21 +93,21 @@ class Program
             scriptname = "user:" + scriptname.Substring(2);
         //if (g_invoking_page.EndsWith("/global.js") && scriptname.ToLower().StartsWith("mediawiki:"))
         //    scriptname = "meta:" + scriptname;
-        if (g_lang == "ru")
-            debug_result += "\n|-\n|[[:" + g_invoking_page + "]]||[[:" + scriptname + "]]";
-        if (user_is_active() && scripts[g_lang].ContainsKey(scriptname))
-            scripts[g_lang][scriptname].active++;
-        else if (user_is_active() && !scripts[g_lang].ContainsKey(scriptname))
-            scripts[g_lang].Add(scriptname, new data() { active = 1, inactive = 0 });
-        else if (!user_is_active() && scripts[g_lang].ContainsKey(scriptname))
-            scripts[g_lang][scriptname].inactive++;
+        if (lang == "ru")
+            debug_result += "\n|-\n|[[:" + invoking_page + "]]||[[:" + scriptname + "]]";
+        if (user_is_active() && scripts[lang].ContainsKey(scriptname))
+            scripts[lang][scriptname].active++;
+        else if (user_is_active() && !scripts[lang].ContainsKey(scriptname))
+            scripts[lang].Add(scriptname, new data() { active = 1, inactive = 0 });
+        else if (!user_is_active() && scripts[lang].ContainsKey(scriptname))
+            scripts[lang][scriptname].inactive++;
         else
-            scripts[g_lang].Add(scriptname, new data() { active = 0, inactive = 1 });
+            scripts[lang].Add(scriptname, new data() { active = 0, inactive = 1 });
     }
     static void process_site(string url)
     {
         string content = "";
-        using (var r = new XmlTextReader(new StringReader(site[g_lang].GetStringAsync(url).Result)))
+        using (var r = new XmlTextReader(new StringReader(site[lang].GetStringAsync(url).Result)))
             while (r.Read())
                 if (r.Name == "page" && r.GetAttribute("_idx") != "-1")
                 {
@@ -176,22 +176,23 @@ class Program
     loader_foreign_rgx = new Regex(@"\.(load|getscript|using)\s*\(\s*['""](https?:|)//([^.]*)\.([^.]*)\.org/w/index\.php\?title=(.*?\.js)", RegexOptions.IgnoreCase),
         loader_foreign2_rgx = new Regex(@"\.(load|getscript|using)\s*\(\s*['""](https?:|)//([^.]*)\.([^.]*)\.org/wiki/([^?]*)\?", RegexOptions.IgnoreCase),
         r1 = new Regex(@"importscript.*\.js", RegexOptions.IgnoreCase), r2 = new Regex(@"\.(load|getscript|using)\b.*\.js", RegexOptions.IgnoreCase), multiline_comment = new Regex(@"/\*.*?\*/", RegexOptions.Singleline);
-    static string g_username, g_lang, g_invoking_page, debug_result = "<center>\n{|class=\"standard sortable\"\n!Страница вызова!!Скрипт";
+    static string username, lang, invoking_page, debug_result = "<center>\n{|class=\"standard sortable\"\n!Страница вызова!!Скрипт";
     static void Main()
     {
         var result = new Dictionary<string, string>() { { "ru", "[[К:Википедия:Статистика и прогнозы]]{{shortcut|ВП:СИС}}<center>Статистика собирается по незакомментированным включениям importScript/" +
                 ".load/.using/.getscript на скриптовых страницах участников рувики, а также их global.js-файлах на Мете. Отсортировано по числу активных участников - сделавших хоть одно действие за " +
-                "последний месяц. Показаны лишь скрипты, имеющие более одного включения. Подробная разбивка скриптов по страницам - [[/details|тут]]. Обновлено " + DateTime.Now.ToString("dd.MM.yyyy")
-                + ". \n{|class=\"standard sortable\"\n!Скрипт!!Активных!!Неактивных!!Всего" }, { "uk", "<center>\n{|class=\"wikitable sortable\"\n!Script!!Active!!Inactive!!Total"} };
+                "последний месяц. Показаны лишь скрипты, имеющие более одного включения. Статистику использования гаджетов см. [[Special:GadgetUsage|тут]]. Подробная разбивка скриптов по страницам - " +
+                "[[/details|тут]]. Обновлено " + DateTime.Now.ToString("dd.MM.yyyy") + ". \n{|class=\"standard sortable\"\n!Скрипт!!Активных!!Неактивных!!Всего" }, { "uk", "<center>\n{|class=\"wikitable " +
+                "sortable\"\n!Script!!Active!!Inactive!!Total"} };
         var resultpage = new Dictionary<string, string>() { { "ru", "ВП:Самые используемые скрипты" }, { "uk", "User:MBH/sandbox" } };
         var w = new StreamWriter("result.txt");
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
 
         foreach (string lang in new string[] { /*"uk",*/ "ru" })
         {
+            Program.lang = lang;
             invoking_pages.Add(lang, new HashSet<string>()); script_users.Add(lang, new HashSet<string>());
             users_activity.Add(lang, new Dictionary<string, bool>()); scripts.Add(lang, new Dictionary<string, data>());
-            g_lang = lang;
             site[lang] = Site(creds[0], creds[1]);
             foreach (string skin in new string[] { "common", "monobook", "vector", "cologneblue", "minerva", "timeless", "simple", "myskin", "modern" })
             {
@@ -212,17 +213,15 @@ class Program
 
             foreach (var invoking_page in invoking_pages[lang])
             {
-                g_username = invoking_page.Substring(invoking_page.IndexOf(':') + 1, invoking_page.IndexOf('/') - 1 - invoking_page.IndexOf(':'));
-                g_invoking_page = invoking_page;
+                username = invoking_page.Substring(invoking_page.IndexOf(':') + 1, invoking_page.IndexOf('/') - 1 - invoking_page.IndexOf(':'));
                 process_site("https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&prop=revisions&rvprop=content&rvlimit=1&titles=" + Uri.EscapeUriString(invoking_page));
-                if (!script_users[g_lang].Contains(g_username))
-                    script_users[g_lang].Add(g_username);
+                if (!script_users[lang].Contains(username))
+                    script_users[lang].Add(username);
             }
 
             foreach (var username in script_users[lang])
             {
-                g_username = username;
-                g_invoking_page = "meta:" + username + "/global.js";
+                invoking_page = "meta:" + username + "/global.js";
                 process_site("https://meta.wikimedia.org/w/api.php?action=query&format=xml&prop=revisions&rvprop=content&rvlimit=1&titles=user:" + Uri.EscapeUriString(username) + "/global.js");
             }
 
