c5afe54f83f44a37eedc58223ffd29c772aa5ca7
Saisengen <64943616+Saisengen@users.noreply.github.com>
2024-12-02 11:45:07 +0700

    Update null-editor.cs

--- incubator/null-editor.cs
+++ incubator/null-editor.cs
@@ -90,7 +90,7 @@ class MyBot : Bot
     }
     public static void Main()
     {
-        Site site = new Site("https://ru.wikipedia.org", creds[8], creds[9]);
+        Site site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
         // счетчик запросов
         MyBot bot = new MyBot();
         string[] set = bot.Settings(6, site);
