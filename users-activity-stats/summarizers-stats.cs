1ceee5b882ccb5db2f132c3d40d6b97a3f7720de
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-01-01 18:52:40 +0700

    Update summarizers-stats.cs

--- users-activity-stats/summarizers-stats.cs
+++ users-activity-stats/summarizers-stats.cs
@@ -67,15 +67,21 @@ class Program
     }
 
     static int position_number = 0;
-    static string resulttext_per_year, resulttext_per_month, user;
+    static string resulttext_per_year, resulttext_per_month, resulttext_alltime, user, common_resulttext = "{{самые активные участники}}{{Плавающая шапка таблицы}}{{shortcut|ВП:ИТОГИ}}<center>\nСтатистика" +
+        " по числу итогов, подведённых %type%. См. также %otherpage%.\n\nСтатистика собирается поиском по тексту страниц обсуждений и потому верна лишь приближённо, нестандартный синтаксис итога или " +
+        "подписи итогоподводящего может привести к тому, что такой итог не будет засчитан. Первично отсортировано по сумме всех итогов, кроме итогов на КУЛ и ЗКП(АУ).\n{|class=\"standard sortable " +
+        "ts-stickytableheader\"\n!№!!Участник!!Σ!!{{vh|[[ВП:КУ|]]}}!!{{vh|[[ВП:ВУС|]]}}!!{{vh|[[ВП:КПМ|]]}}!!{{vh|[[ВП:ПУЗ|]]}}!!{{vh|[[ВП:КОБ|]]+[[ВП:КРАЗД|РАЗД]]}}!!{{vh|[[ВП:ОБК|]]}}!!{{vh|[[ВП:КУЛ|]]" +
+        "}}!!{{vh|[[ВП:ЗКА|]]}}!!{{vh|[[ВП:ОСП|]]+[[ВП:ОАД|]]}}!!{{vh|[[ВП:ЗС|]]}}!!{{vh|[[ВП:ЗС-|]]}}!!{{vh|[[ВП:ЗСП|ЗС]]+[[ВП:ЗСАП|(А)П]]}}!!{{vh|[[ВП:ЗСПИ|]]}}!!{{vh|[[ВП:ЗСФ|]]}}!!{{vh|[[ВП:КОИ|]]}}!!" +
+        "{{vh|[[ВП:ИСЛ|]]}}!!{{vh|[[ВП:ЗКП|]][[ВП:ЗКПАУ|(АУ)]]}}!!{{vh|[[ВП:КИС|]]}}!!{{vh|[[ВП:КИСЛ|]]}}!!{{vh|[[ВП:КХС|]]}}!!{{vh|[[ВП:КЛСХС|]]}}!!{{vh|[[ВП:КДС|]]}}!!{{vh|[[ВП:КЛСДС|]]}}!!{{vh|[[ВП:" +
+        "КИСП|]]}}!!{{vh|[[ВП:КЛСИСП|]]}}!!{{vh|[[ВП:РДБ|]]}}!!{{vh|[[ВП:ФТ|]]+[[ВП:ТЗ|]]}}!!{{vh|[[ВП:Ф-АП|АП]]}}!!{{vh|[[ПРО:ИНК-МР|]]}}";
     static Dictionary<string, Dictionary<string, Dictionary<string, int>>> stats = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>
-    { { "month", new Dictionary<string, Dictionary<string, int>>() }, { "year", new Dictionary<string, Dictionary<string, int>>() } };
-    static string cell (int number)
+    { { "month", new Dictionary<string, Dictionary<string, int>>() }, { "year", new Dictionary<string, Dictionary<string, int>>() }, { "alltime", new Dictionary<string, Dictionary<string, int>>() } };
+    static string cell(int number)
     {
         if (number == 0) return "";
         else return number.ToString();
     }
-    static void writerow(KeyValuePair<string, Dictionary<string, int>> s, bool per_year)
+    static void writerow(KeyValuePair<string, Dictionary<string, int>> s, string type)
     {
         string newrow = "\n|-\n|" + ++position_number + "||{{u|" + s.Key + "}}||" + cell(s.Value["sum"]) + "||" + cell(s.Value["К удалению"]) + "||" + cell(s.Value["К восстановлению"]) + "||" + cell(
             s.Value["К переименованию"]) + "||" + cell(s.Value["Запросы на переименование учётных записей"]) + "||" + cell(s.Value["К объединению"] + s.Value["К разделению"]) + "||" + cell(s.Value[
@@ -87,10 +93,12 @@ class Program
                         s.Value["Хорошие статьи/К лишению статуса"]) + "||" + cell(s.Value["Добротные статьи/Кандидаты"]) + "||" + cell(s.Value["Добротные статьи/К лишению статуса"]) + "||" + cell(s.Value
                         ["Избранные списки и порталы/Кандидаты"]) + "||" + cell(s.Value["Избранные списки и порталы/К лишению статуса"]) + "||" + cell(s.Value["Запросы к ботоводам"]) + "||" + cell(s.Value
                         ["Форум/Архив/Технический"] + s.Value["Технические запросы"]) + "||" + cell(s.Value["Форум/Архив/Авторское право"]) + "||" + cell(s.Value["Инкубатор/Мини-рецензирование"]);
-        if (per_year)
+        if (type == "month")
+            resulttext_per_month += newrow;
+        else if (type == "year")
             resulttext_per_year += newrow;
         else
-            resulttext_per_month += newrow;
+            resulttext_alltime += newrow;
     }
     static void initialize(string type, string pagetype)
     {
@@ -108,6 +116,7 @@ class Program
     static void Main()
     {
         var dtn = DateTime.Now;
+        var alltime = false;
         var lastmonthdate = dtn.AddMonths(-1);
         var lastyear = dtn.AddYears(-1);
         var first_not_fully_summaried_year = new Dictionary<string, int>
@@ -149,7 +158,7 @@ class Program
                 {
                     string pagetitle = pageinfo.title;
                     bool correctpage = false;
-                    int startyear = first_not_fully_summaried_year[pagetype] == 0 ? lastyear.Year : first_not_fully_summaried_year[pagetype];
+                    int startyear = alltime ? 0 : (first_not_fully_summaried_year[pagetype] == 0 ? lastyear.Year : first_not_fully_summaried_year[pagetype]);
                     if (pagetitle.Contains("Избранные"))
                         correctpage = true;
                     else if (yearrgx.IsMatch(pagetitle))
@@ -171,6 +180,7 @@ class Program
                                 user = user.Substring(0, user.IndexOf("/"));
                             if (user == "TextworkerBot")
                                 continue;
+                            initialize("alltime", pagetype);
                             if (signature_year == lastmonthdate.Year && signature_month == lastmonthdate.Month)
                                 initialize("month", pagetype);
                             if (signature_year == lastmonthdate.Year || (signature_year == lastmonthdate.Year - 1 && signature_month > lastmonthdate.Month))
@@ -180,23 +190,25 @@ class Program
                 }
             }
         }
-        string common_resulttext = "{{самые активные участники}}{{Плавающая шапка таблицы}}{{shortcut|ВП:ИТОГИ}}<center>\nСтатистика по числу итогов, подведённых %type%. См. также %otherpage%.\n\n" +
-            "Статистика собирается поиском по тексту страниц обсуждений и потому верна лишь приближённо, нестандартный синтаксис итога или подписи итогоподводящего может привести к тому, что такой " +
-            "итог не будет засчитан. Первично отсортировано по сумме всех итогов, кроме итогов на КУЛ и ЗКП(АУ).\n{|class=\"standard sortable ts-stickytableheader\"\n!№!!Участник!!Σ!!{{vh|[[ВП:КУ|]]}}!!" +
-            "{{vh|[[ВП:ВУС|]]}}!!{{vh|[[ВП:КПМ|]]}}!!{{vh|[[ВП:ПУЗ|]]}}!!{{vh|[[ВП:КОБ|]]+[[ВП:КРАЗД|РАЗД]]}}!!{{vh|[[ВП:ОБК|]]}}!!{{vh|[[ВП:КУЛ|]]}}!!{{vh|[[ВП:ЗКА|]]}}!!{{vh|[[ВП:ОСП|]]+[[ВП:ОАД|]]}}!!" +
-            "{{vh|[[ВП:ЗС|]]}}!!{{vh|[[ВП:ЗС-|]]}}!!{{vh|[[ВП:ЗСП|ЗС]]+[[ВП:ЗСАП|(А)П]]}}!!{{vh|[[ВП:ЗСПИ|]]}}!!{{vh|[[ВП:ЗСФ|]]}}!!{{vh|[[ВП:КОИ|]]}}!!{{vh|[[ВП:ИСЛ|]]}}!!{{vh|[[ВП:ЗКП|]][[ВП:ЗКПАУ|" +
-            "(АУ)]]}}!!{{vh|[[ВП:КИС|]]}}!!{{vh|[[ВП:КИСЛ|]]}}!!{{vh|[[ВП:КХС|]]}}!!{{vh|[[ВП:КЛСХС|]]}}!!{{vh|[[ВП:КДС|]]}}!!{{vh|[[ВП:КЛСДС|]]}}!!{{vh|[[ВП:КИСП|]]}}!!{{vh|[[ВП:КЛСИСП|]]}}!!{{vh|" +
-            "[[ВП:РДБ|]]}}!!{{vh|[[ВП:ФТ|]]+[[ВП:ТЗ|]]}}!!{{vh|[[ВП:Ф-АП|АП]]}}!!{{vh|[[ПРО:ИНК-МР|]]}}";
-        resulttext_per_year = common_resulttext.Replace("%type%", "за последние 12 месяцев").Replace("%otherpage%", "[[ВП:Статистика итогов|итоги за последний месяц]]");
-        resulttext_per_month = common_resulttext.Replace("%type%", "в " + monthnames[lastmonthdate.Month] + " " + lastmonthdate.Year + " года").Replace("%otherpage%", "[[ВП:Статистика итогов/За год|итоги за последние 12 месяцев]]");
-
-        foreach (var s in stats["year"].OrderByDescending(s => s.Value["sum"] - s.Value["К улучшению"] - s.Value["Запросы к патрулирующим от автоподтверждённых участников"] - s.Value["Запросы к патрулирующим"]))
-            writerow(s, true);
-        position_number = 0;
-        foreach (var s in stats["month"].OrderByDescending(s => s.Value["sum"] - s.Value["К улучшению"] - s.Value["Запросы к патрулирующим от автоподтверждённых участников"] - s.Value["Запросы к патрулирующим"]))
-            writerow(s, false);
-
-        Save(site, "ВП:Статистика итогов/За год", resulttext_per_year + "\n|}", "");
-        Save(site, "ВП:Статистика итогов", resulttext_per_month + "\n|}", "");
+        
+        if (alltime)
+        {
+            resulttext_alltime = common_resulttext.Replace("%type%", "за все годы существования Русской Википедии").Replace("%otherpage%", "итоги за [[ВП:Статистика итогов|последний месяц]] и [[ВП:Статистика итогов/За год|год]]");
+            foreach (var s in stats["alltime"].OrderByDescending(s => s.Value["sum"] - s.Value["К улучшению"] - s.Value["Запросы к патрулирующим от автоподтверждённых участников"] - s.Value["Запросы к патрулирующим"]))
+                writerow(s, "alltime");
+            Save(site, "ВП:Статистика итогов/За всё время", resulttext_alltime + "\n|}", "");
+        }
+        else
+        {
+            resulttext_per_month = common_resulttext.Replace("%type%", "в " + monthnames[lastmonthdate.Month] + " " + lastmonthdate.Year + " года").Replace("%otherpage%", "[[ВП:Статистика итогов/За год|итоги за последние 12 месяцев]]");
+            resulttext_per_year = common_resulttext.Replace("%type%", "за последние 12 месяцев").Replace("%otherpage%", "[[ВП:Статистика итогов|итоги за последний месяц]]");
+            foreach (var s in stats["year"].OrderByDescending(s => s.Value["sum"] - s.Value["К улучшению"] - s.Value["Запросы к патрулирующим от автоподтверждённых участников"] - s.Value["Запросы к патрулирующим"]))
+                writerow(s, "year");
+            position_number = 0;
+            foreach (var s in stats["month"].OrderByDescending(s => s.Value["sum"] - s.Value["К улучшению"] - s.Value["Запросы к патрулирующим от автоподтверждённых участников"] - s.Value["Запросы к патрулирующим"]))
+                writerow(s, "month");
+            Save(site, "ВП:Статистика итогов/За год", resulttext_per_year + "\n|}", "");
+            Save(site, "ВП:Статистика итогов", resulttext_per_month + "\n|}", "");
+        }
     }
 }
