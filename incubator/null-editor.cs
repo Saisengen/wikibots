659e27180f6f9a908ec3df59b44b086dc0962932
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-02-23 08:29:48 +0700

    Update null-editor.cs

--- incubator/null-editor.cs
+++ incubator/null-editor.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Web;
 using System.Xml;
 using DotNetWikiBot;
@@ -8,60 +7,12 @@ using DotNetWikiBot;
 class MyBot : Bot
 {
     static string[] creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-    public string[] Settings(byte num, Site site)
-    {
-        string[] ar = new string[num];
-        Page setting = new Page(site, "user:MBH/incubator.js");
-        setting.Load();
-        Regex all = new Regex(@"all.?=.?true", RegexOptions.Singleline);
-        Regex onoff = new Regex(@"nullbot.?=.?true", RegexOptions.Singleline);
-        Regex nullcats = new Regex(@"nullcats.?=.*?" + Regex.Escape(";"), RegexOptions.Singleline);
-        Regex nullpages = new Regex(@"null_pages.?=.*?" + Regex.Escape(";"), RegexOptions.Singleline);
-        Regex nulllinksto = new Regex(@"null_links_to.?=.*?" + Regex.Escape(";"), RegexOptions.Singleline);
-
-        if (all.Matches(setting.text).Count > 0)
-        {
-            if (onoff.Matches(setting.text).Count > 0)
-            {
-                ar[0] = "1";
-                if (nullcats.Matches(setting.text).Count > 0)
-                {
-                    string a = nullcats.Matches(setting.text)[0].ToString();
-                    a = a.Substring(a.IndexOf("=") + 1).Trim();
-                    a = a.Substring(a.IndexOf("\"") + 1);
-                    a = a.Remove(a.IndexOf("\""));
-                    ar[1] = a;
-                }
-                if (nullpages.Matches(setting.text).Count > 0)
-                {
-                    string a = nullpages.Matches(setting.text)[0].ToString();
-                    a = a.Substring(a.IndexOf("=") + 1).Trim();
-                    a = a.Substring(a.IndexOf("\"") + 1);
-                    a = a.Remove(a.IndexOf("\""));
-                    ar[2] = a;
-                }
-                if (nulllinksto.Matches(setting.text).Count > 0)
-                {
-                    string a = nulllinksto.Matches(setting.text)[0].ToString();
-                    a = a.Substring(a.IndexOf("=") + 1).Trim();
-                    a = a.Substring(a.IndexOf("\"") + 1);
-                    a = a.Remove(a.IndexOf("\""));
-                    ar[3] = a;
-                }
-                return ar;
-            }
-            else
-            { ar[0] = "0"; return ar; }
-        }
-        else
-        { ar[0] = "0"; return ar; }
-    }
     public PageList GetCategoryMembers(Site site, string cat, int limit)
     {
         PageList allpages = new PageList(site);
         string[] all = new string[limit];
         int page_num = 0;
-        string URL = site.apiPath + "?action=query&list=categorymembers&cmprop=title&cmlimit=5000&cmtitle=" + HttpUtility.UrlEncode("Категория:" + cat) + "&format=xml";
+        string URL = site.apiPath + "?action=query&list=categorymembers&cmprop=title&cmlimit=5000&cmtitle=" + HttpUtility.UrlEncode("К:" + cat) + "&format=xml";
         string h = site.GetWebPage(URL);
         XmlTextReader rdr = new XmlTextReader(new StringReader(h));
         while (rdr.Read())
@@ -91,64 +42,24 @@ class MyBot : Bot
     public static void Main()
     {
         Site site = new Site("https://ru.wikipedia.org", creds[0], creds[1]);
-        // счетчик запросов
         MyBot bot = new MyBot();
-        string[] set = bot.Settings(6, site);
-        if (set[0] == "1")
+        PageList pl = new PageList(site);
+        foreach (string cat in "Википедия:Статьи со ссылками на элементы Викиданных без русской подписи|Проект:Инкубатор:Запросы на проверку|Проект:Инкубатор:Запросы о помощи|Проект:Инкубатор:Кандидаты на мини-рецензирование|Проект:Инкубатор:Брошенные статьи|Файлы:Несвободные для несуществующих статей|Википедия:К отсроченному удалению|Файлы:Неясный лицензионный статус:Все|Файлы:Неиспользуемые свободные:Все|Файлы:Неиспользуемые несвободные:Все|Википедия:Кандидаты на удаление|Википедия:Статьи с неактуальным шаблоном Не переведено|Статьи со ссылками на отсутствующие файлы".Split('|'))
         {
-            Regex nullcat = new Regex(@"..*?" + Regex.Escape("|"), RegexOptions.Singleline);
-            // объявляем список страниц
-            PageList pl = new PageList(site);
-            foreach (Match m in nullcat.Matches(set[1]))
+            pl = bot.GetCategoryMembers(site, cat, 5000);
+            foreach (Page n in pl)
             {
-                string cat = m.ToString();
-                cat = cat.Remove(cat.Length - 1);
-                pl = bot.GetCategoryMembers(site, cat, 5000);
-                foreach (Page n in pl)
-                {
-                    try
-                    {
-                        n.Load();
-                        n.Save();
-                    }
-                    catch
-                    {
-                        Console.WriteLine(n.title + " can't save or load;\n");
-                    }
-                }
-                pl.Clear();
-            }
-            string pages = set[2];
-            foreach (Match m in nullcat.Matches(pages))
-            {
-                string page = m.ToString();
-                page = page.Remove(page.Length - 1);
-                Page p = new Page(site, page);
-                p.Load();
                 try
-                {
-                    p.Save();
-                }
-                catch
-                {
-                    Console.WriteLine(p.title + " can't save;\n");
-                }
-            }
-            // обрабатываем ccsстраницы...
-            string linksto = set[3];
-            foreach (Match mm in nullcat.Matches(linksto))
-            {
-                string links = mm.ToString();
-                links = links.Remove(links.Length - 1);
-                // заполняем список страниц из категории под номером i
-                pl.FillFromLinksToPage(links);
-                foreach (Page n in pl)
                 {
                     n.Load();
                     n.Save();
                 }
-                pl.Clear();
+                catch
+                {
+                    Console.WriteLine(n.title + " can't save or load;\n");
+                }
             }
+            pl.Clear();
         }
     }
 }
