8d7bcc801acdee03a451d021a968402b921a36ea
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-02-27 19:35:18 +0700

    Update pageview-peaks.cs

--- other-bots/pageview-peaks.cs
+++ other-bots/pageview-peaks.cs
@@ -14,6 +14,27 @@ class result
 }
 class Program
 {
+    static Dictionary<string, string> datespan = new Dictionary<string, string>() { { "month", "{{#expr:31+{{CURRENTDAY}}}}" }, { "year", "{{#expr:365+({{CURRENTWEEK}}-1)*7+{{CURRENTDOW}}}}" } };
+    static Dictionary<string, string> tableheader = new Dictionary<string, string>() { { "ru", "Статья!!Пик!!Медиана!!Дата пика" }, { "uk", "Стаття!!Пік!!Медіана!!Дата піку" },
+        { "be", "Артыкул!!Пік!!Медыяна!!Дата піка" } };
+    static Dictionary<string, string> enddate = new Dictionary<string, string>() { { "01", "31" }, { "02", "29" }, { "03", "31" }, { "04", "30" }, { "05", "31" }, { "06", "30" }, { "07", "31" },
+            { "08", "31" }, { "09", "30" }, { "10", "31" }, { "11", "30" }, { "12", "31" } };
+    static Dictionary<string, Dictionary<string, string>> outputpage = new Dictionary<string, Dictionary<string, string>>
+        { { "uk", new Dictionary<string, string>() { { "month", "Вікіпедія:Спалахи інтересу до статей" }, { "year", "Вікіпедія:Спалахи інтересу до статей/За рік" }, { "total", "Вікіпедія:Спалахи інтересу до статей/За весь час" } } },
+            { "be", new Dictionary<string, string>() { { "month", "Вікіпедыя:Папулярныя артыкулы" }, { "year", "Вікіпедыя:Папулярныя артыкулы/За год" }, { "total", "Вікіпедыя:Папулярныя артыкулы/За ўвесь час" } } },
+            { "ru", new Dictionary<string, string>() { { "month", "ВП:Пики интереса к статьям/За месяц" }, { "year", "ВП:Пики интереса к статьям/За год" }, { "total", "ВП:Пики интереса к статьям/За всё время" } } } };
+    static Dictionary<string, Dictionary<string, int>> minneededpeakvalue = new Dictionary<string, Dictionary<string, int>>
+        { { "ru", new Dictionary<string, int>() { { "month", 10000 }, { "year", 15000 }, { "total", 20000 }, } },
+            { "uk", new Dictionary<string, int>() { { "month", 1000 }, { "year", 2000 }, { "total", 3000 }, } },
+            { "be", new Dictionary<string, int>() { { "month", 15 }, { "year", 30 }, { "total", 100 }, } } };
+    static Dictionary<string, Dictionary<string, string>> monthnames = new Dictionary<string, Dictionary<string, string>>
+        { {"ru", new Dictionary<string, string>() { {"01","января"}, {"02","февраля"}, {"03","марта"}, {"04","апреля"}, {"05","мая"}, {"06","июня"}, {"07","июля"}, {"08","августа"},
+                {"09","сентября"}, {"10","октября"}, {"11","ноября"}, {"12","декабря"} } },
+            {"uk", new Dictionary<string, string>() { {"01","січня"}, {"02","лютого"}, {"03","березня"}, {"04","квітня"}, {"05","травня"}, {"06","червня"}, {"07","липня"}, {"08","серпня"},
+                {"09","вересня"}, {"10","жовтня"}, {"11","листопада"}, {"12","грудня"} } },
+            {"be", new Dictionary<string, string>() { {"01","студзеня"}, {"02","лютага"}, {"03","сакавіка"}, {"04","красавіка"}, {"05","траўня"}, {"06","чэрвеня"}, {"07","ліпеня"}, {"08","жніўня"},
+                {"09","верасня"}, {"10","кастрычніка"}, {"11","лістапада"}, {"12","снежня"} } } };
+    static WebClient cl = new WebClient();
     static HttpClient Site(string wiki, string login, string password)
     {
         var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
@@ -49,49 +70,12 @@ class Program
         else
             Console.WriteLine(result);
     }
-    static void Main()
+    static void process(string mode, string reqstr_period)
     {
-        var cl = new WebClient();
         cl.Headers.Add("user-agent", "Stats grabber of ruwiki user MBH");
-        var results = new Dictionary<string, result>();
-        var enddate = new Dictionary<string, string>() { { "01", "31" }, { "02", "29" }, { "03", "31" }, { "04", "30" }, { "05", "31" }, { "06", "30" }, { "07", "31" }, { "08", "31" }, { "09", "30" }, { "10", "31" }, { "11", "30" }, { "12", "31" } };
-        bool yearly = false;
-        string startmonth, endmonth, datespan;
-        int year = DateTime.Now.AddMonths(-1).Year;
-        var monthnames = new Dictionary<string, Dictionary<string, string>>();
-        monthnames.Add("ru", new Dictionary<string, string>() { {"01","января"}, {"02","февраля"}, {"03","марта"}, {"04","апреля"}, {"05","мая"}, {"06","июня"}, {"07","июля"}, {"08","августа"},
-                {"09","сентября"}, {"10","октября"}, {"11","ноября"}, {"12","декабря"} });
-        monthnames.Add("uk", new Dictionary<string, string>() { {"01","січня"}, {"02","лютого"}, {"03","березня"}, {"04","квітня"}, {"05","травня"}, {"06","червня"}, {"07","липня"}, {"08","серпня"},
-                {"09","вересня"}, {"10","жовтня"}, {"11","листопада"}, {"12","грудня"} });
-        monthnames.Add("be", new Dictionary<string, string>() { {"01","студзеня"}, {"02","лютага"}, {"03","сакавіка"}, {"04","красавіка"}, {"05","траўня"}, {"06","чэрвеня"}, {"07","ліпеня"}, {"08","жніўня"},
-                {"09","верасня"}, {"10","кастрычніка"}, {"11","лістапада"}, {"12","снежня"} });
-        var tableheader = new Dictionary<string, string>() { { "ru", "Статья!!Пик!!Медиана!!Дата пика" }, { "uk", "Стаття!!Пік!!Медіана!!Дата піку" }, { "be", "Артыкул!!Пік!!Медыяна!!Дата піка" } };
-        var outputpage = new Dictionary<string, Dictionary<string, string>>();
-        outputpage.Add("uk", new Dictionary<string, string>() { { "month", "Вікіпедія:Спалахи інтересу до статей" }, { "year", "Вікіпедія:Спалахи інтересу до статей/За рік" }, { "total", "Вікіпедія:Спалахи інтересу до статей/За весь час" } });
-        outputpage.Add("be", new Dictionary<string, string>() { { "month", "Вікіпедыя:Папулярныя артыкулы" }, { "year", "Вікіпедыя:Папулярныя артыкулы/За год" }, { "total", "Вікіпедыя:Папулярныя артыкулы/За ўвесь час" } });
-        outputpage.Add("ru", new Dictionary<string, string>() { { "month", "ВП:Пики интереса к статьям" }, { "year", "ВП:Пики интереса к статьям/За год" }, { "total", "ВП:Пики интереса к статьям/За всё время"} });
-        var minneededpeakvalue = new Dictionary<string, Dictionary<string, int>>();
-        minneededpeakvalue.Add("ru", new Dictionary<string, int>() { { "month", 10000 }, { "year", 15000 }, { "total", 20000 }, });
-        minneededpeakvalue.Add("uk", new Dictionary<string, int>() { { "month", 1000 }, { "year", 2000 }, { "total", 3000 }, });
-        minneededpeakvalue.Add("be", new Dictionary<string, int>() { { "month", 15 }, { "year", 30 }, { "total", 50 }, });
-        foreach (var lang in new HashSet<string>() { "ru", "uk", "be" })
+        foreach (string lang in new HashSet<string>() { "ru", "uk", "be" })
         {
-            results.Clear();
-            if (yearly)
-            {
-                startmonth = "01";
-                endmonth = "12";
-                datespan = "{{#expr:365+({{CURRENTWEEK}}-1)*7+{{CURRENTDOW}}}}";
-            }
-            else
-            {
-                startmonth = DateTime.Now.AddMonths(-1).Month.ToString();
-                if (startmonth.Length == 1)
-                    startmonth = "0" + startmonth;
-                endmonth = startmonth;
-                datespan = "{{#expr:31+{{CURRENTDAY}}}}";
-            }
-
+            var results = new Dictionary<string, result>();
             var creds = new StreamReader("p").ReadToEnd().Split('\n');
             var site = Site(lang, creds[0], creds[1]);
             string cont = "", query = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=allpages&apnamespace=0&apfilterredir=nonredirects&aplimit=max";
@@ -109,8 +93,7 @@ class Program
                             var thispagestats = new Dictionary<string, int>();
                             string currres = "";
                             string reqstr = "";
-                            reqstr = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/" + lang + ".wikipedia/all-access/user/" + Uri.EscapeDataString(page) + "/daily/" + year +
-                                startmonth + "01/" + year + endmonth + enddate[endmonth];
+                            reqstr = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/" + lang + ".wikipedia/all-access/user/" + Uri.EscapeDataString(page) + "/daily/" + reqstr_period;
                             try
                             {
                                 currres = cl.DownloadString(reqstr);
@@ -134,7 +117,7 @@ class Program
                             }
                             var orderedlist = thispagestats.OrderBy(o => o.Value).ToList();
                             int median = orderedlist[orderedlist.Count / 2].Value;
-                            int currentminneededpeakvalue = yearly ? minneededpeakvalue[lang]["year"] : minneededpeakvalue[lang]["month"];
+                            int currentminneededpeakvalue = minneededpeakvalue[lang][mode];
                             if (maxviews >= currentminneededpeakvalue)
                                 results.Add(page, new result() { date = peakdate, max = maxviews, median = median });
                         }
@@ -145,9 +128,23 @@ class Program
             {
                 string month = r.Value.date.Substring(4, 2);
                 string day = r.Value.date.Substring(6, 2);
-                result += "\n|-\n|[[" + r.Key + "]]||{{formatnum:" + r.Value.max + "}}||" + r.Value.median + "||{{~|" + month + day + "}}" + day + " " + monthnames[lang][month];
+                string date = mode == "total" ? r.Value.date.Substring(0, 4) + "-" + month + "-" + day : "{{~|" + month + day + "}}" + day + " " + monthnames[lang][month];
+                result += "\n|-\n|[[" + r.Key + "]]||{{formatnum:" + r.Value.max + "}}||" + r.Value.median + "||" + date;
             }
-            Save(site, lang, yearly ? outputpage[lang]["year"] : outputpage[lang]["month"], result + "\n|}");
+            Save(site, lang, outputpage[lang][mode], result + "\n|}");
+        }
+    }
+    static void Main()
+    {
+        int year_of_previous_month = DateTime.Now.AddMonths(-1).Year;
+        string lastmonth = DateTime.Now.AddMonths(-1).ToString("MM");
+        if (lastmonth != "12")
+            process("month", year_of_previous_month + lastmonth + "01/" + year_of_previous_month + lastmonth + enddate[lastmonth]);
+        else
+        {
+            process("year", year_of_previous_month + "0101/" + year_of_previous_month + "1231");
+            process("month", year_of_previous_month + lastmonth + "01/" + year_of_previous_month + lastmonth + enddate[lastmonth]);
+            process("total", "20150701/" + year_of_previous_month + "1231");
         }
     }
 }
