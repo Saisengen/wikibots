e65c41f5ad65ec51dc59df98f0ecc7bde05dc860
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-03-27 00:20:16 +0700

    Update and rename ip_map.cs to ip-map.cs

--- other-bots/ip-map.cs
+++ other-bots/ip-map.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -8,6 +9,18 @@ class Program
     static Regex iprgx = new Regex(@"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$");
     static void Main()
     {
+        var ww = new StreamWriter("res.txt");
+        var rangeinfo = new Dictionary<int, string>();
+        foreach (var line in new StreamReader("ipv4_ranges.txt").ReadToEnd().Replace("\r", "").Split('\n'))
+            rangeinfo.Add(Convert.ToByte(line.Split('\t')[0]), line.Split('\t')[1]);
+        for (int o1 = 0; o1 < 224; o1++)
+            for (int o2 = 0; o2 < 256; o2++)
+            {
+                int big1 = o1 / 16; int smol1 = o1 % 16; int big2 = o2 / 16; int smol2 = o2 % 16;
+                ww.WriteLine("<div class=\"s\" style=\"top:" + (big1 * 16 + big2) * 8 + "px;left:" + (smol1 * 16 + smol2) * 8 + "px;width:8px;height:8px;\" data-info=\"" + o1 + "." + o2 + ".X.X<br>" +
+                    rangeinfo[o1] + "\"></div>");
+            }
+        ww.Close();
         foreach (var lang in new StreamReader("ipmap_src.txt").ReadLine().Split('|'))
         {
             var creds = new StreamReader("p").ReadToEnd().Split('\n');
@@ -47,25 +60,29 @@ class Program
                     byte intensity = raw_data.GetPixel(w, h).G;
                     true_colors.SetPixel(w, h, getcolor(intensity));
                     if (intensity == 0)
-                        high_contrast.SetPixel(w, h, Color.FromArgb(0, 0, 0));
+                        high_contrast.SetPixel(w, h, Color.Black);
                     else
-                        high_contrast.SetPixel(w, h, Color.FromArgb(255, 255, 255));
+                        high_contrast.SetPixel(w, h, Color.White);
                         
                 }
             Graphics high = Graphics.FromImage(high_contrast), truec = Graphics.FromImage(true_colors);
-            high.DrawString(lang + ".wikipedia", new Font("Arial", 400), Brushes.White, 500, 3500); truec.DrawString(lang + ".wikipedia", new Font("Arial", 400), Brushes.White, 500, 3500);
+            high.DrawString(lang + ".wikipedia", new Font("Arial", 400), Brushes.White, 450, 3500); truec.DrawString(lang + ".wikipedia", new Font("Arial", 400), Brushes.White, 450, 3500);
             high_contrast.Save("high contrast " + lang + ".png", System.Drawing.Imaging.ImageFormat.Png); true_colors.Save("true colors " + lang + ".png", System.Drawing.Imaging.ImageFormat.Png);
         }
     }
     static Color getcolor (byte intensity)
     {
-        if (intensity < 64)
-            return Color.FromArgb(0, 0, intensity * 4);
-        else if (intensity < 128)
-            return Color.FromArgb(0, (intensity - 64) * 4, 255 - ((intensity - 64) * 4));
-        else if (intensity < 192)
-            return Color.FromArgb((intensity - 128) * 4, 255 - ((intensity - 128) * 4), 0);
+        if (intensity == 0)
+            return Color.Blue;
+        if (intensity < 51)
+            return Color.FromArgb(0, intensity * 5, 255);
+        else if (intensity < 102)
+            return Color.FromArgb(0, 255, 255 - (intensity - 51) * 5);
+        else if (intensity < 153)
+            return Color.FromArgb((intensity - 102) * 5, 255, 0);
+        else if (intensity < 204)
+            return Color.FromArgb(255, 255 - (intensity - 153) * 5, 0);
         else
-            return Color.FromArgb(intensity, (intensity - 192) * 4, (intensity - 192) * 4);
+            return Color.FromArgb(255, (intensity - 204) * 5, (intensity - 204) * 5);
     }
 }
