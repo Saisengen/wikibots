2019aac33fb8ba7b46591927d5033b69a0004983
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-06-03 09:42:28 +0700

    Update everyday-job.cs

--- other-bots/everyday-job.cs
+++ other-bots/everyday-job.cs
@@ -151,9 +151,6 @@ class Program
             string pagetext = site.GetStringAsync("https://ru.wikipedia.org/wiki/" + Uri.EscapeDataString(file) + "?action=raw").Result;
             Save(site, file, "{{subst:nld}}\n" + pagetext, "вынос на КБУ файла без валидной лицензии");
         }
-
-        if (autocatfiles.Count != 0)
-            Save(site, "К:Файлы:Неясный лицензионный статус от " + dtn.Day + " " + monthname[dtn.Month] + " " + dtn.Year, "[[К:Файлы:Неясный лицензионный статус|" + dtn.ToString("MM-dd") + "]]", "");
     }
     static void orphan_nonfree_files()
     {
@@ -249,8 +246,6 @@ class Program
             if (DateTime.Now - DateTime.ParseExact(uploaddate, "yyyy-MM-dd", CultureInfo.InvariantCulture) > new TimeSpan(0, 1, 0, 0))
                 Save(site, file, "{{subst:ofud}}\n" + file_descr, "вынос на КБУ неиспользуемого в статьях несвободного файла");
         }
-        if (unused_files.Count != 0)
-            Save(site, "К:Файлы:Неиспользуемые несвободные от " + dtn.Day + " " + monthname[dtn.Month] + " " + dtn.Year, "__NOGALLERY__\n[[К:Файлы:Неиспользуемые несвободные|" + dtn.ToString("MM-dd") + "]]", "");
     }
     static void redirs_deletion()
     {
@@ -786,7 +781,7 @@ class Program
             "страниц"] + "||" + cats["Википедия:Статьи для переименования"] + "||" + cats["Википедия:Незакрытые обсуждения переименования страниц"] + "||" + cats["Википедия:Кандидаты на объединение"] +
             "||" + cats["Википедия:Незакрытые обсуждения объединения страниц"] + "||" + cats["Википедия:Статьи для разделения"] + "||" + cats["Википедия:Незакрытые обсуждения разделения страниц"] + "||" +
             non_summaried_vus.Matches(vus_text).Count + "||" + cats["Википедия:Незакрытые обсуждения восстановления страниц"] + "||" + cats["Инкубатор:Все статьи"] + "||" + cats["Инкубатор:Статьи на " +
-            "мини-рецензировании"] + "||" + cats["Инкубатор:Запросы на проверку"] + "||" + cats["Инкубатор:Запросы о помощи"] + "\n|}";
+            "мини-рецензировании"] + "||" + cats["Инкубатор:Запросы на проверку"] + "||" + cats["Инкубатор:Запросы о помощи"];
         Save(site, "Участник:MBH/Завалы", stat_text + result, "");
     }
     static void inc_check_help_requests()
@@ -829,6 +824,131 @@ class Program
                 Save(site, pagename, pagetext, "добавлен {{В инкубаторе}}, если не было, и [[User:IncubatorBot/Скрытие категорий и интервик|скрыты категории]], если были");
             }
     }
+    //static void img_inc_bot()
+    //{
+    //    string[,] imgs = new string[5000, 9];
+    //    int m = 0;
+    //    using (var r = new XmlTextReader(new StringReader(site.GetStringAsync("https://ru.wikipedia.org/w/api.php?action=query&list=categorymembers&format=xml&cmprop=title&cmtitle=К:Инкубатор:Все статьи").Result)))
+    //        while (r.Read())
+    //            if (r.Name == "cm" && r.GetAttribute("ns") == "102")
+    //            {
+    //                string title = r.GetAttribute("title");
+    //                current_page.Load();
+    //                List<string> str = current_page.GetImages();
+    //                string im = "";
+    //                int i;
+    //                if (str.Count > 0)
+    //                {
+    //                    for (i = 0; i < str.Count; i++)
+    //                    {
+    //                        if (str[i].Contains("Файл:[[Файл:"))
+    //                            str[i] = str[i].Replace("Файл:[[Файл:", "Файл:");
+    //                        if (str[i] != "Файл:Example.jpg" & str[i] != "Файл:Person.jpg" & str[i] != "Файл:")
+    //                            im = im + str[i] + "|";
+    //                    }
+    //                    if (im.Length > 1)
+    //                    {
+    //                        im = im.Remove(im.Length - 1);
+    //                        if (string.IsNullOrEmpty(im)) // API запрос
+    //                            throw new WikiBotException(Bot.Msg("No title specified for page to load."));
+    //                        try
+    //                        {
+    //                            var reader = new XmlTextReader(new StringReader(site.GetWebPage(site.apiPath + "?action=query&prop=imageinfo&iiprop=timestamp|user|size|dimensions&titles=" + HttpUtility.UrlEncode(im) + "&format=xml")));
+    //                            while (reader.Read())
+    //                                if (reader.NodeType == XmlNodeType.Element)
+    //                                {
+    //                                    if (reader.Name == "page")
+    //                                    {
+    //                                        m++;
+    //                                        imgs[m, 0] = reader.GetAttribute("title");
+    //                                        imgs[m, 1] = reader.GetAttribute("imagerepository");
+    //                                        imgs[m, 7] = current_page.title;
+    //                                        if (imgs[m, 1] == "" ^ imgs[m, 1] == null)
+    //                                            m--;
+    //                                    }
+    //                                    if (reader.Name == "ii")
+    //                                    {
+    //                                        imgs[m, 2] = reader.GetAttribute("timestamp");
+    //                                        imgs[m, 3] = reader.GetAttribute("user");
+    //                                        imgs[m, 4] = reader.GetAttribute("width");
+    //                                        imgs[m, 5] = reader.GetAttribute("height");
+    //                                    }
+    //                                }
+    //                        }
+    //                        catch { continue; }
+    //                    }
+    //                }
+    //            }
+    //    for (int n = 1; n < m + 1; n++)
+    //    {
+    //        bool exist = true;
+    //        string ptext = "";
+    //        if (imgs[n, 1] == "local")
+    //        { Page temp = new Page(site, imgs[n, 0]); temp.Load(); ptext = temp.text; }
+    //        else if (imgs[n, 1] == "shared")
+    //        {
+    //            string file = imgs[n, 0].Replace("Файл", "File");
+    //            Page temp = new Page(commons, file); temp.Load(); ptext = temp.text;
+    //        }
+    //        else { exist = false; }
+    //        if (exist == true)
+    //        {
+    //            imgs[n, 6] = "";
+    //            Regex CC = new Regex("{{.*?CC.*?}}", RegexOptions.IgnoreCase);
+    //            Regex GFDL = new Regex("{{.*?(GFDL|LGPL|GPL).*?}}", RegexOptions.IgnoreCase);
+    //            Regex PD = new Regex("{{(Not-PD|PD).*?}}", RegexOptions.IgnoreCase);
+    //            Regex FU = new Regex("{{(Несвободный файл|FU|Fairuse|Символ|Скриншот).*?}}", RegexOptions.Singleline);
+    //            Regex FoP = new Regex("{{FoP.*?}}", RegexOptions.IgnoreCase);
+    //            Regex VRT = new Regex("{{.*?(OTRS|VRT).*?}}", RegexOptions.IgnoreCase);
+    //            Regex Attribution = new Regex("{{Attribution.*?}}", RegexOptions.IgnoreCase);
+    //            Regex no = new Regex("{{no .*?}}", RegexOptions.IgnoreCase);
+    //            Regex other = new Regex("{{(VI.com-Gerbovnik|FAL|MTL|BSD|Trivial|Свободный скриншот|Kremlin).*?}}", RegexOptions.IgnoreCase);
+    //            Regex comm = new Regex("{{(Apache|ADRM|AGPL|APL|Artistic|BArch|Beerware|C0|CDDL|CPL|Careware|Copyright|DSL|EPL|Expat|FOLP|FWL|MIT|MPL|MTL|OAL|Open|WTFPL|X11|Zlib).*?}}", RegexOptions.IgnoreCase);
+    //            imgs[n, 9] = "0";
+    //            if (!VRT.IsMatch(ptext)) // if OTRS - somebody has already check file, so we don't need to check it again
+    //            {
+    //                if (!no.IsMatch(ptext, 0)) // the same, if here is template {{no permission}} (npd, nad, nld etc), file was checked before
+    //                {
+    //                    imgs[n, 9] = "1";
+    //                    if (CC.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + CC.Matches(ptext)[0].ToString();
+    //                    if (GFDL.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + GFDL.Matches(ptext)[0].ToString();
+    //                    if (PD.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + PD.Matches(ptext)[0].ToString();
+    //                    if (FU.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + FU.Matches(ptext)[0].ToString();
+    //                    if (FoP.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + FoP.Matches(ptext)[0].ToString();
+    //                    if (Attribution.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + Attribution.Matches(ptext)[0].ToString();
+    //                    if (other.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + other.Matches(ptext)[0].ToString();
+    //                    if (comm.IsMatch(ptext))
+    //                        imgs[n, 6] = imgs[n, 6] + comm.Matches(ptext)[0].ToString();
+    //                }
+    //            }
+    //        }
+    //    }
+    //    p.text = "{{Проект:Инкубатор/Шаблон навигации}}\n<div align=\"right\">'''Последнее обновление:''' ~~~~~ </div> \n\n{| class=\"wikitable sortable\"\n|-\n! Файл\n! Дата\n! Автор\n! Место\n! Размеры\n! Лицензия\n! Статья\n! Статус\n";
+    //    DateTime now = DateTime.UtcNow;
+    //    // дата и время последней правки
+    //    int term = 120;
+    //    for (int n = 1; n < m + 1; n++)
+    //    {
+    //        DateTime datefile = DateTime.Parse(imgs[n, 2]);
+    //        TimeSpan diff = now - datefile;
+    //        if (imgs[n, 9] == "1")
+    //        {
+    //            if (diff.Days < term)
+    //            {
+    //                if (imgs[n, 0].IndexOf("=") != -1) { imgs[n, 0] = imgs[n, 0].Replace("=", "%3D"); } // поправить - не работает
+    //                p.text = p.text + "{{User:IncubatorBot/img|" + imgs[n, 0] + "|" + imgs[n, 2] + "|" + imgs[n, 3] + "|" + imgs[n, 1] + "|" + imgs[n, 4] + "x" + imgs[n, 5] + "|<nowiki>" + imgs[n, 6] + "</nowiki>|" + imgs[n, 7] + "|" + imgs[n, 8] + "}}\n";
+    //            }
+    //        }
+    //    }
+    //    p.Save("обновление списка", true);
+    //}
     static void Main()
     {
         creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
@@ -837,6 +957,7 @@ class Program
         monthname = new string[13] { "", "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
         try { inc_check_help_requests(); } catch(Exception e) { Console.WriteLine(e.ToString()); }
         try { stat_bot(); } catch (Exception e) { Console.WriteLine(e.ToString()); }
+        //try { img_inc_bot(); } catch (Exception e) { Console.WriteLine(e.ToString()); }
         try { main_inc_bot(); } catch (Exception e) { Console.WriteLine(e.ToString()); }
         try { orphan_nonfree_files(); } catch (Exception e) { Console.WriteLine(e.ToString()); }
         try { unlicensed_files(); } catch (Exception e) { Console.WriteLine(e.ToString()); }
