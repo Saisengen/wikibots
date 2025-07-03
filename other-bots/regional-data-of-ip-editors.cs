baaa6ab311621de4441b9c3f28281deed50d9b1c
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-07-03 07:12:45 +0700

    Update regional-data-of-ip-editors.cs

--- other-bots/regional-data-of-ip-editors.cs
+++ other-bots/regional-data-of-ip-editors.cs
@@ -9,9 +9,9 @@ using System.Threading;
 using System.Xml;
 using Newtonsoft.Json;
 public class Root {
-    public string status, country, region, regionName, city, zip, timezone, isp, org, @as, query; public double lat, lon; public int edits; }
+    public string status, country, countryCode, region, regionName, city, zip, timezone, isp, org, @as, query; public double lat, lon; public Dictionary<int, int> edits; }
 public class regdata {
-    public string country; public int edits; }
+    public string cntrCode; public Dictionary<int, int> edits; }
 class Program
 {
     static void save(string title, string text)
@@ -34,34 +34,38 @@ class Program
         var client = new HttpClient(); var ranges24 = new Dictionary<string, Root>(); var regions = new Dictionary<string, regdata>();
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n'); site = Site("ru", creds[0], creds[1]);
         foreach (string lang in "ru".Split('|'))
-        {
-            string cont = "", query = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=allrevisions&arvprop=user&arvlimit=max&arvend=2025-06-25T00:00:00";
-            while (cont != null)
-                using (var r = new XmlTextReader(new StringReader(cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arvcontinue=" + cont).Result)))
+            for (int year = 2025; year < 2026; year++)
+            {
+                string query = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=allrevisions&arvprop=user&arvlimit=max&arvend=" + year + "-01-01T00:00:00&&arvstart=" + year +
+                    "-12-31T23:59:59", cont = "";
+                while (cont != null)
                 {
-                    r.Read(); r.Read(); r.Read(); cont = r.GetAttribute("arvcontinue"); while (r.Read())
+                    var r = new XmlTextReader(new StringReader(cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arvcontinue=" + cont).Result)); r.Read(); r.Read(); r.Read();
+                    cont = r.GetAttribute("arvcontinue"); while (r.Read())
                         if (r.Name == "rev" && r.GetAttribute("anon") == "")
                         {
                             string user = r.GetAttribute("user");
                             if (iprgx.IsMatch(user))
                             {
-                                string ip = iprgx.Match(user).Groups[1].Value + ".0";
-                                if (ranges24.ContainsKey(ip)) ranges24[ip].edits++; else ranges24.Add(ip, new Root() { edits = 1 } );
+                                var octets = iprgx.Match(user).Groups[1].Value.Split('.'); byte o1, o2, o3; try { o1 = Convert.ToByte(octets[0]); o2 = Convert.ToByte(octets[1]); o3 = Convert.ToByte(octets[2]); }
+                                catch { continue; } string range = new string(new char[] { (char)o1, (char)o2, (char)o3 });
+                                if (ranges24.ContainsKey(range)) ranges24[range].edits[year]++; else ranges24.Add(range, new Root() { edits = new Dictionary<int, int> { { year, 1 } } });
                             }
                         }
                 }
-            Console.WriteLine("ips=" + ranges24.Count); int c = 0;
-            foreach (var range in ranges24) try
-                {
-                    Root data = JsonConvert.DeserializeObject<Root>(client.GetStringAsync("http://ip-api.com/json/" + range.Key).Result); Thread.Sleep(1450); if (++c % 1000 == 0) Console.WriteLine(c);
-                    if (data.regionName != null)
-                        if (!regions.ContainsKey(data.regionName)) regions.Add(data.regionName, new regdata() { edits = range.Value.edits, country = data.country});
-                        else regions[data.regionName].edits += range.Value.edits;
-                } catch (Exception e) { Console.WriteLine(e.ToString()); }
-            string result = "<center>\n{|class=\"standard sortable\"\n!Страна!!Регион!!Правок";
-            foreach (var region in regions.OrderByDescending(t => t.Value.edits))
-                result += "\n|-\n|" + region.Value.country + "||" + region.Key + "||" + region.Value.edits;
-            save("u:MBH/Черновик", result + "\n|}");
-        }
+                Console.WriteLine("ips=" + ranges24.Count); int c = 0;
+                foreach (var range in ranges24) try
+                    {
+                        Root data = JsonConvert.DeserializeObject<Root>(client.GetStringAsync("http://ip-api.com/json/" + range.Key).Result); Thread.Sleep(1450); if (++c % 1000 == 0) Console.WriteLine(c);
+                        if (data.regionName != null)
+                            if (!regions.ContainsKey(data.regionName)) regions.Add(data.regionName, new regdata() { edits = range.Value.edits, cntrCode = data.countryCode });
+                            else regions[data.regionName].edits[year] += range.Value.edits[year];
+                    }
+                    catch (Exception e) { Console.WriteLine(e.ToString()); }
+            }
+        string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!Правок\n|-\n!!2025";
+        foreach (var region in regions.OrderByDescending(t => t.Value.edits))
+            result += "\n|-\n|{{флаг|" + region.Value.cntrCode + "}} " + region.Key + "||" + region.Value.edits[2025];
+        save("u:MBH/Черновик", result + "\n|}");
     }
 }
