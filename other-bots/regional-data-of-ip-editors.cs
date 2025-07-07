a219e6bb90725a0c414b26dd33d5b96a1053a403
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-07-07 18:28:17 +0700

    Update regional-data-of-ip-editors.cs

--- other-bots/regional-data-of-ip-editors.cs
+++ other-bots/regional-data-of-ip-editors.cs
@@ -9,9 +9,9 @@ using System.Threading;
 using System.Xml;
 using Newtonsoft.Json;
 public class Root {
-    public string status, country, countryCode, region, regionName, city, zip, timezone, isp, org, @as, query; public double lat, lon; public Dictionary<int, int> edits; }
+    public string country, regionName; public Dictionary<int, int> edits; }
 public class regdata {
-    public string cntrCode; public Dictionary<int, int> edits; }
+    public string country; public Dictionary<int, int> edits; }
 class Program
 {
     static void save(string title, string text)
@@ -34,7 +34,7 @@ class Program
         var client = new HttpClient(); var ranges24 = new Dictionary<string, Root>(); var regions = new Dictionary<string, regdata>();
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n'); site = Site("ru", creds[0], creds[1]);
         foreach (string lang in "ru".Split('|'))
-            for (int year = 2025; year < 2026; year++)
+            for (int year = 2020; year < 2026; year++)
             {
                 string query = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=allrevisions&arvprop=user&arvlimit=max&arvend=" + year + "-01-01T00:00:00&&arvstart=" + year +
                     "-12-31T23:59:59", cont = "";
@@ -47,8 +47,8 @@ class Program
                             string user = r.GetAttribute("user");
                             if (iprgx.IsMatch(user))
                             {
-                                var octets = iprgx.Match(user).Groups[1].Value.Split('.'); byte o1, o2, o3; try { o1 = Convert.ToByte(octets[0]); o2 = Convert.ToByte(octets[1]); o3 = Convert.ToByte(octets[2]); }
-                                catch { continue; } string range = new string(new char[] { (char)o1, (char)o2, (char)o3 });
+                                //var octets = iprgx.Match(user).Groups[1].Value.Split('.'); byte o1, o2, o3; try { o1 = Convert.ToByte(octets[0]); o2 = Convert.ToByte(octets[1]); o3 = Convert.ToByte(octets[2]); }
+                                /*catch { continue; }*/ string range = /*new string(new char[] { (char)o1, (char)o2, (char)o3 })*/iprgx.Match(user).Groups[1].Value;
                                 if (ranges24.ContainsKey(range)) ranges24[range].edits[year]++; else ranges24.Add(range, new Root() { edits = new Dictionary<int, int> { { year, 1 } } });
                             }
                         }
@@ -56,16 +56,18 @@ class Program
                 Console.WriteLine("ips=" + ranges24.Count); int c = 0;
                 foreach (var range in ranges24) try
                     {
-                        Root data = JsonConvert.DeserializeObject<Root>(client.GetStringAsync("http://ip-api.com/json/" + range.Key).Result); Thread.Sleep(1450); if (++c % 1000 == 0) Console.WriteLine(c);
+                        //string ip = (byte)range.Key[0] + "." + (byte)range.Key[1] + "." + (byte)range.Key[2] + ".0";
+                        Root data = JsonConvert.DeserializeObject<Root>(client.GetStringAsync("http://ip-api.com/json/" + range.Key + ".0").Result); Thread.Sleep(1425); if (++c % 2000 == 0) Console.WriteLine(c);
                         if (data.regionName != null)
-                            if (!regions.ContainsKey(data.regionName)) regions.Add(data.regionName, new regdata() { edits = range.Value.edits, cntrCode = data.countryCode });
+                            if (!regions.ContainsKey(data.regionName)) regions.Add(data.regionName, new regdata() { edits = range.Value.edits, country = data.country });
                             else regions[data.regionName].edits[year] += range.Value.edits[year];
                     }
-                    catch (Exception e) { Console.WriteLine(e.ToString()); }
+                    catch (Exception e) { Console.WriteLine("exception=" + e.ToString()); }
             }
-        string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!Правок\n|-\n!!2025";
-        foreach (var region in regions.OrderByDescending(t => t.Value.edits))
-            result += "\n|-\n|{{флаг|" + region.Value.cntrCode + "}} " + region.Key + "||" + region.Value.edits[2025];
+        string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!colspan=6|Правок в рувики\n|-\n!2020!!2021!!2022!!2023!!2024!!2025!!Всего";
+        foreach (var r in regions.OrderByDescending(r => r.Value.edits[2020] + r.Value.edits[2021] + r.Value.edits[2022] + r.Value.edits[2023] + r.Value.edits[2024] + r.Value.edits[2025]))
+            result += "\n|-\n|{{флаг|" + r.Value.country + "}} " + r.Key + "||" + r.Value.edits[2020] + "||" + r.Value.edits[2021] + "||" + r.Value.edits[2022] + "||" + r.Value.edits[2023] + "||" + 
+                r.Value.edits[2024] + "||" + r.Value.edits[2025] + "||" + (r.Value.edits[2020] + r.Value.edits[2021] + r.Value.edits[2022] + r.Value.edits[2023] + r.Value.edits[2024] + r.Value.edits[2025]);
         save("u:MBH/Черновик", result + "\n|}");
     }
 }
