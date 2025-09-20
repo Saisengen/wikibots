c833d5378037dca13f2ec39863e6300dd436d9c8
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-09-20 20:56:09 +0700

    Update deleted-files-exclusion.cs

--- other-bots/deleted-files-exclusion.cs
+++ other-bots/deleted-files-exclusion.cs
@@ -17,65 +17,35 @@ class logrecord
 }
 class Program
 {
-    static string[] creds;
-    static HttpClient ru, commons;
-    static DateTime dtn = DateTime.UtcNow;
-    static string univ_query;
+    static string[] creds; static HttpClient ru, commons; static DateTime dtn = DateTime.UtcNow; static string univ_query;
     static void Main()
     {
         creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n');
-        ru = Site("ru.wikipedia", creds[0], creds[1]);
-        commons = Site("commons.wikimedia", creds[0], creds[1]);
+        ru = Site("ru.wikipedia", creds[0], creds[1]); commons = Site("commons.wikimedia", creds[0], creds[1]);
         univ_query = "https://{domain}.org/w/api.php?action=query&format=xml&list=logevents&leprop=title|user|comment&leaction=delete/delete&leend=" +
             dtn.AddHours(-2).ToString("yyyy-MM-ddTHH:mm:ss") + "&lenamespace=6&lelimit=max";
-        run_ru();
-        run_commons();
+        run_ru(); run_commons();
     }
-    static string e(string src)
-    {
-        return Uri.EscapeDataString(src);
-    }
+    static string e(string src) { return Uri.EscapeDataString(src); } 
     static HttpClient Site(string wiki, string login, string password)
     {
-        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() });
-        client.DefaultRequestHeaders.Add("User-Agent", login);
-        var result = client.GetAsync("https://" + wiki + ".org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result;
-        if (!result.IsSuccessStatusCode)
-            return null;
-        var doc = new XmlDocument();
-        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
-        var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
-        result = client.PostAsync("https://" + wiki + ".org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password }, { "lgtoken", logintoken }, { "format", "xml" } })).Result;
-        if (!result.IsSuccessStatusCode)
-            return null;
-        return client;
+        var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() }); client.DefaultRequestHeaders.Add("User-Agent", login);
+        var result = client.GetAsync("https://" + wiki + ".org/w/api.php?action=query&meta=tokens&type=login&format=xml").Result; var doc = new XmlDocument();
+        doc.LoadXml(result.Content.ReadAsStringAsync().Result); var logintoken = doc.SelectSingleNode("//tokens/@logintoken").Value;
+        result = client.PostAsync("https://" + wiki + ".org/w/api.php", new FormUrlEncodedContent(new Dictionary<string, string> { { "action", "login" }, { "lgname", login }, { "lgpassword", password },
+            { "lgtoken", logintoken } })).Result; return client;
     }
     static void Save(HttpClient site, string title, string text, string comment)
     {
-        var doc = new XmlDocument();
-        var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result;
-        if (!result.IsSuccessStatusCode)
-            return;
-        doc.LoadXml(result.Content.ReadAsStringAsync().Result);
-        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value;
-        var request = new MultipartFormDataContent();
-        request.Add(new StringContent("edit"), "action");
-        request.Add(new StringContent(title), "title");
-        request.Add(new StringContent(text), "text");
-        request.Add(new StringContent(comment), "summary");
-        request.Add(new StringContent(token), "token");
-        request.Add(new StringContent("xml"), "format");
-        result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
-        if (!result.ToString().Contains("uccess"))
-            Console.WriteLine(result);
+        var doc = new XmlDocument(); var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result; doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+        var token = doc.SelectSingleNode("//tokens/@csrftoken").Value; var request = new MultipartFormDataContent { { new StringContent("edit"), "action" }, { new StringContent(title), "title" },
+            { new StringContent(text), "text" }, { new StringContent(comment), "summary" }, { new StringContent(token), "token" } }; result = site.PostAsync("https://ru.wikipedia.org/w/api.php", request).Result;
+        if (!result.ToString().Contains("uccess")) Console.WriteLine(result);
     }
     static void delete_transclusion(pair dp, bool isCommons)
     {
-        string initial_text = ru.GetStringAsync("https://ru.wikipedia.org/wiki/" + e(dp.page) + "?action=raw").Result;
-        string new_page_text = initial_text;
-        string filename = dp.file.Substring(5);
-        string rgxtext = filename.Replace(" ", "[ _]+");
-        rgxtext = "(" + rgxtext + "|" + e(filename.Substring(5)) + ")";
+        string initial_text = ru.GetStringAsync("https://ru.wikipedia.org/wiki/" + e(dp.page) + "?action=raw").Result; string new_page_text = initial_text;
+        string filename = dp.file[4] == ':' ? dp.file.Substring(5) : dp.file; string rgxtext = filename.Replace(" ", "[ _]+"); rgxtext = "(" + rgxtext + "|" + e(filename) + ")";
         var r1 = new Regex(@" *\[\[\s*(file|image|файл|изображение):\s*" + rgxtext + @"[^[\]]*\]\]", RegexOptions.IgnoreCase);
         var r2 = new Regex(@" *\[\[\s*(file|image|файл|изображение):\s*" + rgxtext + @"[^[]*(\[\[[^\[\]]*\]\][^[\]]*)*\]\]", RegexOptions.IgnoreCase);
         var r3 = new Regex(@" *<\s*gallery[^>]*>\s*(file|image|файл|изображение):\s*" + rgxtext + @"[^\n]*<\s*/gallery\s*>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
@@ -84,31 +54,19 @@ class Program
         var r6 = new Regex(@" *<\s*gallery[^>]*>\s*<\s*/gallery\s*>", RegexOptions.IgnoreCase | RegexOptions.Singleline);
         var r7 = new Regex(@" *\{\{\s*(flagicon image|audio)[^|}]*\|\s*" + rgxtext + @"[^}]*\}\}");
         var r8 = new Regex(@" *([=|]\s*)(file|image|файл|изображение):\s*" + rgxtext, RegexOptions.IgnoreCase);
-        var r9 = new Regex(@" *([=|]\s*)" + rgxtext, RegexOptions.IgnoreCase);
-        new_page_text = r1.Replace(new_page_text, "");
-        new_page_text = r2.Replace(new_page_text, "");
-        new_page_text = r3.Replace(new_page_text, "");
-        new_page_text = r4.Replace(new_page_text, "$1");
-        new_page_text = r5.Replace(new_page_text, "$1");
-        new_page_text = r6.Replace(new_page_text, "");
-        new_page_text = r7.Replace(new_page_text, "");
-        new_page_text = r8.Replace(new_page_text, "$1");
-        new_page_text = r9.Replace(new_page_text, "$1");
+        var r9 = new Regex(@" *([=|]\s*)" + rgxtext, RegexOptions.IgnoreCase); new_page_text = r1.Replace(new_page_text, "");
+        new_page_text = r2.Replace(new_page_text, ""); new_page_text = r3.Replace(new_page_text, ""); new_page_text = r4.Replace(new_page_text, "$1"); new_page_text = r5.Replace(new_page_text, "$1");
+        new_page_text = r6.Replace(new_page_text, ""); new_page_text = r7.Replace(new_page_text, ""); new_page_text = r8.Replace(new_page_text, "$1"); new_page_text = r9.Replace(new_page_text, "$1");
         if (new_page_text != initial_text)
-            try
-            {
+            try {
                 string comment = "[[file:" + filename + "]] удалён [[user:" + dp.deletion_data.deleter + "]] по причине " + dp.deletion_data.comment;
                 Save(ru, dp.page, new_page_text, isCommons ? comment.Replace("[[", "[[c:") : comment);
-                if (dp.page.StartsWith("Шаблон:"))
-                {
+                if (dp.page.StartsWith("Шаблон:")) {
                     string logpage_text = ru.GetStringAsync("https://ru.wikipedia.org/wiki/u:MBH/Шаблоны с удалёнными файлами?action=raw").Result;
                     Save(ru, "u:MBH/Шаблоны с удалёнными файлами", logpage_text + "\n* [[" + dp.page + "]]", "");
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            catch (Exception e) { Console.WriteLine(e.ToString()); }
     }
     static void find_and_delete_usages(Dictionary<string, logrecord> deletedfiles, bool iscommons)
     {
@@ -158,8 +116,7 @@ class Program
                 r.WhitespaceHandling = WhitespaceHandling.None;
                 r.Read(); r.Read(); r.Read(); cont = r.GetAttribute("lecontinue");
                 while (r.Read())
-                    if (r.Name == "item")
-                    {
+                    if (r.Name == "item") {
                         string title = r.GetAttribute("title");
                         if (title == null) continue;
                         string comment = r.GetAttribute("comment") ?? "";
@@ -214,37 +171,23 @@ class Program
                 string filename = "";
                 while (r.Read())
                 {
-                    if (r.NodeType == XmlNodeType.Element && r.Name == "page")
-                    {
+                    if (r.NodeType == XmlNodeType.Element && r.Name == "page") {
                         filename = r.GetAttribute("title");
                         file_exists = r.GetAttribute("_idx")[0] != '-';
                     }
-                    if (r.Name == "fu" && !file_exists && Convert.ToInt16(r.GetAttribute("ns")) % 2 == 0)
-                    {
+                    if (r.Name == "fu" && !file_exists && Convert.ToInt16(r.GetAttribute("ns")) % 2 == 0) {
                         var page = r.GetAttribute("title");
                         string initial_text = ru.GetStringAsync("https://ru.wikipedia.org/wiki/" + e(page) + "?action=raw").Result;
                         string newname;
-                        try
-                        {
-                            newname = inner_link_to_replacement_file.Match(replacedfiles[rf].comment).Groups[3].Value;
-                        }
-                        catch
-                        {
-                            newname = commons_importer_link.Match(replacedfiles[rf].comment).Groups[1].Value;
-                        }
+                        try { newname = inner_link_to_replacement_file.Match(replacedfiles[rf].comment).Groups[3].Value; }
+                        catch { newname = commons_importer_link.Match(replacedfiles[rf].comment).Groups[1].Value; }
                         string rgxtext = filename.Substring(5).Replace(" ", "[ _]");
                         rgxtext = "(" + rgxtext + "|" + e(filename.Substring(5)) + ")";
                         var rgx = new Regex(rgxtext, RegexOptions.IgnoreCase);
                         string new_page_text = rgx.Replace(initial_text, newname);
                         if (new_page_text != initial_text)
-                            try
-                            {
-                                Save(ru, page, new_page_text, "[[" + rf + "]] удалён [[u:" + replacedfiles[rf].deleter + "]] по причине " + replacedfiles[rf].comment);
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e.ToString());
-                            }
+                            try { Save(ru, page, new_page_text, "[[" + rf + "]] удалён [[u:" + replacedfiles[rf].deleter + "]] по причине " + replacedfiles[rf].comment); }
+                            catch (Exception e) { Console.WriteLine(e.ToString()); }
                     }
                 }
             }
