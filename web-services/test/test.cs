169f1a6d542f1532b87b55ac3a9a58bae82c91d2
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-10-04 11:51:08 +0700

    Update test.cs

--- web-services/test/test.cs
+++ web-services/test/test.cs
@@ -1,21 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 internal class Program
 {
     static void Main()
     {
-        Thread.Sleep(6*60*1000);
-        Console.WriteLine("Content-type: text/html\n");
-        var allvars = Environment.GetEnvironmentVariables();
-        var dict = new Dictionary<string, string>();
+        Console.WriteLine("Content-type: text/html\n"); var allvars = Environment.GetEnvironmentVariables(); var dict = new Dictionary<string, string>();
         Console.WriteLine("<body><table border=1><tr><th>Env var</th><th>Value</th><tr>");
         foreach (var v in allvars.Keys)
             dict.Add(v.ToString(), allvars[v].ToString());
-
         foreach (var record in dict.OrderBy(d => d.Key))
-            Console.WriteLine("<tr><td>" + record.Key + "</td><td>" + ((record.Key.Contains("PASS") || record.Key.Contains("CRED")) ? "" : record.Value) + "</td></tr>\n");
+            Console.WriteLine("<tr><td>" + record.Key + "</td><td>" + ((record.Key.Contains("PASS") || record.Key.Contains("CONN_STRING")) ? "" : record.Value) + "</td></tr>\n");
         Console.WriteLine("</table></body>");
     }
 }
