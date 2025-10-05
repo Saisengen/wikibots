53ba0fa337f537c070ad249047c7ec82dcbb8ca3
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-10-05 11:37:07 +0700

    Update regional-data-of-ip-editors.cs

--- other-bots/regional-data-of-ip-editors.cs
+++ other-bots/regional-data-of-ip-editors.cs
@@ -8,7 +8,7 @@ using System.Text.RegularExpressions;
 using System.Xml;
 using System.Threading;
 using Newtonsoft.Json;
-public class json { public string country, regionName; }
+public class json { public string country, regionName; } public class slicedata { public long reg, ip4, ip6; }
 class Program
 {
     static string cell(int number) { if (number == 0) return ""; else return number.ToString(); }
@@ -36,10 +36,11 @@ class Program
     static void Main()
     {
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n'); var site = Site(creds[0], creds[1]);
-        var iprgx = new Regex(@"^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}$"); string lang = "ur"; var resulttable = new Dictionary<string, Dictionary<int, int>>(); int startyear = 2002, endyear = 2025;
+        var iprgx = new Regex(@"^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}$"); string lang = "zh"; var resulttable = new Dictionary<string, Dictionary<int, int>>(); int startyear = 2002, endyear = 2025;
+        var total = new Dictionary<int, slicedata>();
         for (int year = startyear; year <= endyear; year = year + 2) {
             string query = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=allrevisions&arvprop=user&arvlimit=max&arvend=" + year + "-01-01T00:00:00&&arvstart=" + (year + 1) +
-                "-12-31T23:59:59", cont = "";
+                "-12-31T23:59:59", cont = ""; Console.WriteLine(year); total.Add(year, new slicedata { reg = 0, ip4 = 0, ip6 = 0 });
             while (cont != null) {
                 var r = new XmlTextReader(new StringReader(cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arvcontinue=" + cont).Result)); r.Read(); r.Read(); r.Read();
                 cont = r.GetAttribute("arvcontinue"); while (r.Read())
@@ -47,24 +48,32 @@ class Program
                         string user = r.GetAttribute("user");
                         if (IPAddress.TryParse(user, out IPAddress address)) {
                             string range = "";
-                            if (iprgx.IsMatch(user)) range = iprgx.Match(user).Groups[1].Value + ".0";
+                            if (iprgx.IsMatch(user)) { range = iprgx.Match(user).Groups[1].Value + ".0"; total[year].ip4++; }
                             else if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6) {
-                                byte[] bytes = address.GetAddressBytes(); for (int i = 4; i < 16; i++) bytes[i] = 0; range = new IPAddress(bytes).ToString(); }
+                                byte[] bytes = address.GetAddressBytes(); for (int i = 4; i < 16; i++) bytes[i] = 0; range = new IPAddress(bytes).ToString(); total[year].ip6++; }
                             if (range != "") {
                                 string region = range2region(range);
-                                if (!resulttable.ContainsKey(region))
-                                {
-                                    resulttable.Add(region, new Dictionary<int, int>());
+                                if (!resulttable.ContainsKey(region)) {
+                                    resulttable.Add(region, new Dictionary<int, int>()); resulttable[region].Add(0, 0);
                                     for (int y = startyear; y <= endyear; y = y + 2)
                                         resulttable[region].Add(y, 0);
                                 }
-                                resulttable[region][year]++; resulttable[region][0]++;                                
+                                resulttable[region][year]++; resulttable[region][0]++;
                             }
-                        }
+                        } else total[year].reg++;
                     }
             }
         }
-        string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!colspan=13|Правок в этой вики\n|-\n!02-03!!04-05!!06-07!!08-09!!10-11!!12-13!!14-15!!16-17!!18-19!!20-21!!22-23!!24-25!!Всего";
+        string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!colspan=13|Правок в этой вики\n|-\n!02-03!!04-05!!06-07!!08-09!!10-11!!12-13!!14-15!!16-17!!18-19!!20-21!!22-23!!24-25!!" +
+            "Всего\n|-\n|Всего правок, тыс.";
+        for (int year = startyear; year <= endyear; year = year + 2)
+            result += "||" + (total[year].reg + total[year].ip4 + total[year].ip6) / 1000;
+        result += "\n|-\n|Доля анонимных";
+        for (int year = startyear; year <= endyear; year = year + 2)
+            result += "||" + Math.Round((double)(total[year].ip4 + total[year].ip6) * 100 / (total[year].reg + total[year].ip4 + total[year].ip6), 3) + "%";
+        result += "\n|-\n|Доля IPv6 в анонимных";
+        for (int year = startyear; year <= endyear; year = year + 2)
+            result += "||" + Math.Round((double)total[year].ip6 * 100 / (total[year].ip4 + total[year].ip6), 3) + "%";
         foreach (var fullregion in resulttable.OrderByDescending(r => r.Value[0])) {
             string region = fullregion.Key.Split('!')[1]; string country = fullregion.Key.Split('!')[0];
             result += "\n|-\n|{{flag|" + country + "}} " + region;
