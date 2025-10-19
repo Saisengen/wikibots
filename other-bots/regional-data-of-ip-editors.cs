b10af5a947420dd2d9e7d848167b82f684b838ff
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-10-19 23:13:38 +0700

    Update regional-data-of-ip-editors.cs

--- other-bots/regional-data-of-ip-editors.cs
+++ other-bots/regional-data-of-ip-editors.cs
@@ -30,50 +30,59 @@ class Program
     static string range2region(string range)
     {
         if (ranges2regions.ContainsKey(range)) return ranges2regions[range];
-        else try { json data = JsonConvert.DeserializeObject<json>(client.GetStringAsync("http://ip-api.com/json/" + range).Result); Thread.Sleep(1500); string answer = data.country + "!" + data.regionName;
+        else try { json data = JsonConvert.DeserializeObject<json>(client.GetStringAsync("http://ip-api.com/json/" + range).Result); Thread.Sleep(1490); string answer = data.country + "!" + data.regionName;
                 ranges2regions.Add(range, answer); return answer; } catch (Exception e) { Console.WriteLine("exception=" + e.ToString()); return null; }
     }
     static void Main()
     {
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n'); var site = Site(creds[0], creds[1]);
-        var iprgx = new Regex(@"^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}$"); string lang = "zh"; var resulttable = new Dictionary<string, Dictionary<int, int>>(); int startyear = 2002, endyear = 2025;
-        var total = new Dictionary<int, slicedata>();
+        var iprgx = new Regex(@"^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}$"); string lang = "id"; var resulttable = new Dictionary<string, Dictionary<int, int>>(); int startyear = 2002, endyear = 2025;
+        var total = new Dictionary<int, slicedata>(); total.Add(0, new slicedata { reg = 0, ip4 = 0, ip6 = 0 });
         for (int year = startyear; year <= endyear; year = year + 2) {
             string query = "https://" + lang + ".wikipedia.org/w/api.php?action=query&format=xml&list=allrevisions&arvprop=user&arvlimit=max&arvend=" + year + "-01-01T00:00:00&&arvstart=" + (year + 1) +
                 "-12-31T23:59:59", cont = ""; Console.WriteLine(year); total.Add(year, new slicedata { reg = 0, ip4 = 0, ip6 = 0 });
             while (cont != null) {
                 var r = new XmlTextReader(new StringReader(cont == "" ? site.GetStringAsync(query).Result : site.GetStringAsync(query + "&arvcontinue=" + cont).Result)); r.Read(); r.Read(); r.Read();
                 cont = r.GetAttribute("arvcontinue"); while (r.Read())
-                    if (r.Name == "rev" && r.GetAttribute("anon") == "") {
-                        string user = r.GetAttribute("user");
-                        if (IPAddress.TryParse(user, out IPAddress address)) {
-                            string range = "";
-                            if (iprgx.IsMatch(user)) { range = iprgx.Match(user).Groups[1].Value + ".0"; total[year].ip4++; }
-                            else if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6) {
-                                byte[] bytes = address.GetAddressBytes(); for (int i = 4; i < 16; i++) bytes[i] = 0; range = new IPAddress(bytes).ToString(); total[year].ip6++; }
-                            if (range != "") {
-                                string region = range2region(range);
-                                if (!resulttable.ContainsKey(region)) {
-                                    resulttable.Add(region, new Dictionary<int, int>()); resulttable[region].Add(0, 0);
-                                    for (int y = startyear; y <= endyear; y = y + 2)
-                                        resulttable[region].Add(y, 0);
+                    if (r.Name == "rev")
+                        if (r.GetAttribute("anon") == "") {
+                            string user = r.GetAttribute("user");
+                            if (IPAddress.TryParse(user, out IPAddress address)) {
+                                string range = "";
+                                if (iprgx.IsMatch(user)) { range = iprgx.Match(user).Groups[1].Value + ".0"; total[year].ip4++; total[0].ip4++; }
+                                else if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6) {
+                                    byte[] bytes = address.GetAddressBytes(); for (int i = 4; i < 16; i++) bytes[i] = 0; range = new IPAddress(bytes).ToString(); total[year].ip6++; total[0].ip6++;
                                 }
-                                resulttable[region][year]++; resulttable[region][0]++;
+                                if (range != null && range != "") {
+                                    string region = range2region(range);
+                                    if (!resulttable.ContainsKey(region)) {
+                                        resulttable.Add(region, new Dictionary<int, int>()); resulttable[region].Add(0, 0);
+                                        for (int y = startyear; y <= endyear; y = y + 2)
+                                            resulttable[region].Add(y, 0);
+                                    }
+                                    resulttable[region][year]++; resulttable[region][0]++;
+                                }
                             }
-                        } else total[year].reg++;
-                    }
+                        }
+                        else { total[year].reg++; total[0].reg++; }
             }
         }
         string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!colspan=13|Правок в этой вики\n|-\n!02-03!!04-05!!06-07!!08-09!!10-11!!12-13!!14-15!!16-17!!18-19!!20-21!!22-23!!24-25!!" +
             "Всего\n|-\n|Всего правок, тыс.";
         for (int year = startyear; year <= endyear; year = year + 2)
-            result += "||" + (total[year].reg + total[year].ip4 + total[year].ip6) / 1000;
+            result += "||" + (total[year].reg + total[year].ip4 + total[year].ip6 == 0 ? "" : ((total[year].reg + total[year].ip4 + total[year].ip6) / 1000).ToString());
+        result += "||" + (total[0].reg + total[0].ip4 + total[0].ip6) / 1000;
+
         result += "\n|-\n|Доля анонимных";
         for (int year = startyear; year <= endyear; year = year + 2)
-            result += "||" + Math.Round((double)(total[year].ip4 + total[year].ip6) * 100 / (total[year].reg + total[year].ip4 + total[year].ip6), 3) + "%";
+            result += "||" + (total[year].reg + total[year].ip4 + total[year].ip6 == 0 ? "" : Math.Round((double)(total[year].ip4 + total[year].ip6) * 100 / (total[year].reg + total[year].ip4 + total[year].ip6), 3) + "%");
+        result += "||" + Math.Round((double)(total[0].ip4 + total[0].ip6) * 100 / (total[0].reg + total[0].ip4 + total[0].ip6), 3) + "%";
+
         result += "\n|-\n|Доля IPv6 в анонимных";
         for (int year = startyear; year <= endyear; year = year + 2)
-            result += "||" + Math.Round((double)total[year].ip6 * 100 / (total[year].ip4 + total[year].ip6), 3) + "%";
+            result += "||" + (total[year].reg + total[year].ip4 + total[year].ip6 == 0 ? "" : Math.Round((double)total[year].ip6 * 100 / (total[year].ip4 + total[year].ip6), 3) + "%");
+        result += "||" + Math.Round((double)total[0].ip6 * 100 / (total[0].ip4 + total[0].ip6), 3) + "%";
+
         foreach (var fullregion in resulttable.OrderByDescending(r => r.Value[0])) {
             string region = fullregion.Key.Split('!')[1]; string country = fullregion.Key.Split('!')[0];
             result += "\n|-\n|{{flag|" + country + "}} " + region;
@@ -82,7 +91,7 @@ class Program
             result += "||" + cell(fullregion.Value[0]);
         }
         result += "\n|}";
-        var w = new StreamWriter(lang + ".txt"); w.Write(JsonConvert.SerializeObject(resulttable)); w.Close();
+        try { var w = new StreamWriter(lang + endyear + ".txt"); w.Write(JsonConvert.SerializeObject(resulttable)); w.Close(); } catch { }
         save(site, "ВП:Геопозиция анонимных правщиков/" + lang + "wiki", result);
     }
 }
