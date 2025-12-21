0aee025d478fc11cd4b849ba2d7ee9afd89636dd
Saisengen <64943616+Saisengen@users.noreply.github.com>
2025-12-21 22:40:28 +0700

    Update review.cs

--- web-services/review/review.cs
+++ web-services/review/review.cs
@@ -5,16 +5,14 @@ using System.Web;
 using System.Text.RegularExpressions;
 class Program
 {
-    static void sendresponse(string project, bool onlycat, string cat, string result)
-    {
+    static void sendresponse(string project, bool onlycat, string cat, string result) {
         string answer = new StreamReader(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "review.html")).ReadToEnd().Replace("%result%", result)
             .Replace("%project%", project).Replace("%cat%", cat);
         if (onlycat)
             answer = answer.Replace("%checked%", "checked");
         Console.WriteLine(answer);
     }
-    static void Main()
-    {
+    static void Main() {
         string input = Environment.GetEnvironmentVariable("QUERY_STRING");
         if (input == "" || input == null) {
             sendresponse("ru.wikipedia", false, "", "");
@@ -25,11 +23,9 @@ class Program
         string cat = parameters["cat"];
         bool onlycat = parameters["cat"] == "1";
         
-        var client = new HttpClient(); var ids = new Regex(@"revid=""(\d*)"" stable_revid=""(\d*)""");
-        var results = ids.Matches(client.GetStringAsync("https://" + project + ".org/w/api.php?action=query&format=xml&list=oldreviewedpages&ordir=older&orlimit=50").Result);
+        var client = new HttpClient(); var title_rgx = new Regex(@"title=""[^""]*""");
+        var titles = title_rgx.Matches(client.GetStringAsync("https://" + project + ".org/w/api.php?action=query&format=xml&list=oldreviewedpages&ordir=older&orlimit=20").Result);
         var a = new Random();
-        var choice = results[a.Next(results.Count)];
-        var oldid = choice.Groups[2].Value; var currid = choice.Groups[1].Value;
-        sendresponse(project, onlycat, cat, "<iframe src=\"https://" + project + ".org/w/index.php?title=Навальный,_Алексей_Анатольевич&diff=" + currid + "&oldid=" + oldid + "\" height=1000 width=90%></iframe>");
+        sendresponse(project, onlycat, cat, "<iframe src=\"https://" + project + ".org/wiki/" + Uri.EscapeDataString(titles[a.Next(titles.Count)].Groups[1].Value) + "></iframe>");
     }
 }
