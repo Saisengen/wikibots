cec9e789038afe9e74d8a443cde95a6132682465
Saisengen <64943616+Saisengen@users.noreply.github.com>
2026-01-07 21:58:54 +0700

    Update regional-data-of-ip-editors.cs

--- other-bots/regional-data-of-ip-editors.cs
+++ other-bots/regional-data-of-ip-editors.cs
@@ -14,7 +14,7 @@ class Program
     static string cell(int number) { if (number == 0) return ""; else return number.ToString(); } static double round(double input) { return Math.Round(input, 1); }
     static Dictionary<string, string> ranges2regions = new Dictionary<string, string>(); static HttpClient client = new HttpClient(); static HttpClient site;
     static Dictionary<int, slicedata> total = new Dictionary<int, slicedata>() { { 0, new slicedata { reg = 0, ip4 = 0, ip6 = 0 } } }; static Regex iprgx = new Regex(@"^(\d{1,3}\.\d{1,3}\.\d{1,3})\.\d{1,3}$");
-    static Dictionary<string, Dictionary<int, int>> resulttable = new Dictionary<string, Dictionary<int, int>>(); static string lang = "zh"; static int startyear = 2008, endyear = 2013;
+    static Dictionary<string, Dictionary<int, int>> resulttable = new Dictionary<string, Dictionary<int, int>>(); static string lang = "pt"; static int startyear = 2014, endyear = 2017;
     static void save(HttpClient site, string title, string text)
     {
         var doc = new XmlDocument(); var result = site.GetAsync("https://ru.wikipedia.org/w/api.php?action=query&format=xml&meta=tokens&type=csrf").Result; if (!result.IsSuccessStatusCode) return;
@@ -31,15 +31,18 @@ class Program
     }
     static string range2region(string range)
     {
-        string answer = "null";
         if (ranges2regions.ContainsKey(range))
             return ranges2regions[range];
         else try {
-                answer = client.GetStringAsync("http://ip-api.com/json/" + range).Result;
-                json data = JsonConvert.DeserializeObject<json>(answer); Thread.Sleep(1480);
+                json data = JsonConvert.DeserializeObject<json>(client.GetStringAsync("http://ip-api.com/json/" + range).Result); Thread.Sleep(1470);
                 string result = data.country + "!" + data.regionName;
                 ranges2regions.Add(range, result); return result;
-            } catch { Console.WriteLine("http://ip-api.com/json/" + range + " => " + answer); return null; }
+            }
+            catch (Exception e) {
+                ranges2regions.Add(range, "null");
+                Console.WriteLine("http://ip-api.com/json/" + range + "\n" + e.ToString());
+                return "null";
+            }
     }
     static void initialize_resulttable_row(string region)
     {
@@ -53,7 +56,7 @@ class Program
     {
         for (int y = 2002; y <= 2024; y = y + 2)
             total.Add(y, new slicedata { reg = 0, ip4 = 0, ip6 = 0 });
-        foreach (int part in new int[4] { 2007, 2013, 2019, 2025 })
+        foreach (int part in new int[] { 2007, 2013, 2017, 2023, 2025 })
         {
             var temp_table = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<int, int>>>(new StreamReader(lang + part + ".txt").ReadToEnd());
             foreach (var region in temp_table.Keys) {
@@ -68,8 +71,7 @@ class Program
             }
             total[0].reg += temp_total[0].reg; total[0].ip4 += temp_total[0].ip4; total[0].ip6 += temp_total[0].ip6;
         }
-        string result = "<center>\n{|class=\"standard sortable\"\n!rowspan=2|Регион!!colspan=13|Правок в этой вики\n|-\n!02-03!!04-05!!06-07!!08-09!!10-11!!12-13!!14-15!!16-17!!18-19!!20-21!!22-23!!24-25!!" +
-            "Всего\n|-\n|Всего правок, тыс.";
+        string result = "{{шапка геотаблиц}}\n|Всего правок, тыс.";
         for (int year = 2002; year <= 2024; year = year + 2)
             result += "||" + (total[year].reg + total[year].ip4 + total[year].ip6 == 0 ? "" : ((total[year].reg + total[year].ip4 + total[year].ip6) / 1000).ToString());
         result += "||" + (total[0].reg + total[0].ip4 + total[0].ip6) / 1000;
@@ -113,10 +115,8 @@ class Program
                                 }
                                 if (range != null && range != "") {
                                     string region = range2region(range);
-                                    if (region != null) {
-                                        initialize_resulttable_row(region);
-                                        resulttable[region][year]++; resulttable[region][0]++;
-                                    }
+                                    initialize_resulttable_row(region);
+                                    resulttable[region][year]++; resulttable[region][0]++;
                                 }
                             }
                         }
@@ -129,7 +129,7 @@ class Program
     static void Main()
     {
         var creds = new StreamReader((Environment.OSVersion.ToString().Contains("Windows") ? @"..\..\..\..\" : "") + "p").ReadToEnd().Split('\n'); site = Site(creds[0], creds[1]);
-        merge();
-        //read_part();        
+        //merge();
+        read_part();        
     }
 }
